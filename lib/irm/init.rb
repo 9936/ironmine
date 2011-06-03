0fcf696528784ec0234716cc0d55df0ce249ee75
hailor <hailor1985@gmail.com>
2011-06-03 09:46:08 +0800

    初始化审批错误类

--- lib/irm/init.rb
+++ lib/irm/init.rb
@@ -1,3 +1,14 @@
 # IRM模块初始化脚本
 #注册IRM模块菜单
 Irm::MenuManager.reset_menu
+
+module Wf
+  class Error < ::StandardError; end
+  class ApproverError < Error
+
+  end
+
+  class MissingSelectApproverError < ApproverError; end
+  class MissingDefaultApproverError < ApproverError; end
+  class MissingAutoApproverError < ApproverError; end
+end
