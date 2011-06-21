35ad8d9e7aa73be7dcf8542aafa13d5e27c96048
Stanley Liu <stanleyliu@Stanley-LiumatoMacBook-Pro.local>
2011-06-21 12:25:59 +0800

    为delayed_job增加日志记录功能

--- lib/irm/extends_log_delayed_job.rb
+++ lib/irm/extends_log_delayed_job.rb
@@ -5,14 +5,14 @@
         after_save  :after_deal
          def after_deal
           if @recordBool == true
-             @djr = Irm::DelayedJobRecord.new
-             @djr.delayed_job_id = self.id
-             @djr.priority = self.priority
-             @djr.status = 0
-             @djr.run_at = self.run_at
-             @djr.run_by = "host:#{Socket.gethostname} pid:#{Process.pid}"
-             @djr.handler = self.handler
-             @djr.save
+#             @djr = Irm::DelayedJobRecord.new
+#             @djr.delayed_job_id = self.id
+#             @djr.priority = self.priority
+#             @djr.status = 0
+#             @djr.run_at = self.run_at
+#             @djr.run_by = "host:#{Socket.gethostname} pid:#{Process.pid}"
+#             @djr.handler = self.handler
+#             @djr.save
              @recordBool = false
 
           end
