b783239f07eb1f3d6fd9da8a5739394fde0e4be6
hailor <hailor1985@gmail.com>
2011-06-23 16:52:22 +0800

    根据设定的规则生成自定义id

--- lib/irm/custom_id.rb
+++ lib/irm/custom_id.rb
@@ -6,7 +6,7 @@ module Irm::CustomId
       private
       def setup_custom_id
         id_column = self.class.columns.detect{|i| i.primary}
-        if id_column&&id_column.type.eql?(:string)&&id_column.limit==24
+        if id_column&&id_column.type.eql?(:string)&&id_column.limit>21
           self.id = Irm::IdGenerator.instance.generate(self.class.table_name)
         end
       end
