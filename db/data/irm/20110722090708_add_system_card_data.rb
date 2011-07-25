ba807b4d0f4e603959bb2583584090eb965cf073
Stanley Liu <stanleyliu@Stanley-LiumatoMacBook-Pro.local>
2011-07-25 09:50:33 +0800

    kanban 展现

--- db/data/irm/20110722090708_add_system_card_data.rb
+++ db/data/irm/20110722090708_add_system_card_data.rb
@@ -8,11 +8,12 @@ class AddSystemCardData < ActiveRecord::Migration
                                :card_url => "incident_requests/{{id}}/journals/new",
                                :title_attribute_name => "title",
                                :description_attribute_name => "summary",
-                               :date_attribute_name => "ir_updated_at",
-                               :status_code => Irm::Constant::ENABLED)
+                               :date_attribute_name => "ij_updated_at",
+                               :status_code => Irm::Constant::ENABLED,:not_auto_mult=>true)
     irm_card_1.cards_tls.build(:language=>'zh',:source_lang=>'en',:name=>'客户回复后的事故单',:description=>'客户回复后的事故单')
     irm_card_1.cards_tls.build(:language=>'en',:source_lang=>'en',:name=>'Customer Replied',:description=>'Customer Replied')
     irm_card_1.save
+
 
     irm_card_2 = Irm::Card.new(:card_code => "IR_ARR_WAITING_FOR_REPLY",
                                :background_color => "#b3eae4",
@@ -22,7 +23,7 @@ class AddSystemCardData < ActiveRecord::Migration
                                :title_attribute_name => "title",
                                :description_attribute_name => "summary",
                                :date_attribute_name => "updated_at",
-                               :status_code => Irm::Constant::ENABLED)
+                               :status_code => Irm::Constant::ENABLED,:not_auto_mult=>true)
     irm_card_2.cards_tls.build(:language=>'zh',:source_lang=>'en',:name=>'新到达的事故单',:description=>'新到达的事故单')
     irm_card_2.cards_tls.build(:language=>'en',:source_lang=>'en',:name=>'New IR',:description=>'New IR')
     irm_card_2.save
