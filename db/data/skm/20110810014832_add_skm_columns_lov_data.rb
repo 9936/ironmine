7e9c79f6e06299075411e8b2cc7f3167ccd031ca
Stanley Liu <stanleyliu@Stanley-LiumatoMacBook-Pro.local>
2011-08-10 10:25:22 +0800

    fix skm column lov data migration

--- db/data/skm/20110810014832_add_skm_columns_lov_data.rb
+++ db/data/skm/20110810014832_add_skm_columns_lov_data.rb
@@ -6,9 +6,9 @@ class AddSkmColumnsLovData < ActiveRecord::Migration
                                       :query_clause => "", :id_column => "id", :value_column => "name",
                                       :value_column_width => "80", :desc_column =>"description",
                                       :desc_column_width => "120", :listable_flag => "N",:not_auto_mult=>true)
-    skm_column.skm_columns_tls.build(:language=>'zh',:source_lang=>'en',:name=>'知识库栏目',:description=>'知识库栏目',
+    skm_column.list_of_values_tls.build(:language=>'zh',:source_lang=>'en',:name=>'知识库栏目',:description=>'知识库栏目',
                                      :value_title => "栏目名", :desc_title => "描述")
-    skm_column.skm_columns_tls.build(:language=>'en',:source_lang=>'en',:name=>'Skm Columns',:description=>'Skm Columns',
+    skm_column.list_of_values_tls.build(:language=>'en',:source_lang=>'en',:name=>'Skm Columns',:description=>'Skm Columns',
                                      :value_title => "Column Name", :desc_title => "Description")
   end
 
