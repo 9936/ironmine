73974c6fc4eb8c1d39ed676dd3b0c019c3930af0
hailor <hailor1985@gmail.com>
2011-08-17 17:28:58 +0800

    应用密码策略验证用户密码

--- db/migrate/irm/20110817073019_add_password_policy_to_person.rb
+++ db/migrate/irm/20110817073019_add_password_policy_to_person.rb
@@ -3,9 +3,6 @@ class AddPasswordPolicyToPerson < ActiveRecord::Migration
     add_column :irm_people,:password_updated_at,:datetime,:after=>:last_assigned_date
     add_column :irm_people,:locked_time,:integer,:default=>0,:after=>:last_assigned_date
     add_column :irm_people,:locked_until_at,:datetime,:after=>:last_assigned_date
-    Irm::Person.all.each  do |p|
-      p.update_attribute(:password_updated_at,p.created_at)
-    end
   end
 
   def self.down
