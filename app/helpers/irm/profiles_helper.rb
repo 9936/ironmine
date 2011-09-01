c2638b78c8446742afca6ac0ede68a45a1786131
hailor <hailor1985@gmail.com>
2011-09-01 11:04:43 +0800

    在简档查看页面不显示没有功能的功能分区

--- app/helpers/irm/profiles_helper.rb
+++ app/helpers/irm/profiles_helper.rb
@@ -1,6 +1,9 @@
 module Irm::ProfilesHelper
-  def grouped_functions
+  def grouped_functions(function_ids=[])
     fs = Irm::Function.multilingual.enabled.with_function_group(I18n.locale).where(:public_flag=>Irm::Constant::SYS_NO,:login_flag=>Irm::Constant::SYS_NO)
+    if function_ids.any?
+      fs.delete_if{|i| !function_ids.include?(i.id)}
+    end
     fs.group_by{|i| i[:zone_code]}
   end
 
