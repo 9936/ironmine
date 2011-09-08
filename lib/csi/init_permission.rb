ae8aac0bd12fac7de300d598859720ed1996363d
Stanley Liu <stanleyliu@Stanley-LiumatoMacBook-Pro.local>
2011-09-08 11:01:53 +0800

    fixed: 移除用户所属的支持组时系统报错

--- lib/csi/init_permission.rb
+++ lib/csi/init_permission.rb
@@ -9,7 +9,7 @@ Irm::AccessControl.map do |map|
                              "csi/survey_subjects" => [:new, :create],
                              "csi/survey_ranges" => [:new, :create]}
   map.function :edit_survey, {"csi/surveys" => [:edit, :update,:active],
-                              "csi/survey_subjects" => [:edit, :update],
+                              "csi/survey_subjects" => [:edit, :update,:destroy],
                               "csi/survey_ranges" => [:edit, :update]}
   map.function :view_survey_result, {"csi/surveys" => [:export_result, :show_result, :survey_report]}
   map.function :reply_survey, {"csi/surveys" => [:reply, :create_result,:show_reply,:thanks,:password]}
