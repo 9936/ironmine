63c4b8c0c93ad21a664f1a30eee43ccde2655e58
hailor <hailor1985@gmail.com>
2011-10-02 16:32:59 +0800

    使用jquery改写原来的cascadeselect和menubutton

--- public/javascripts/jquery-plugin.js
+++ public/javascripts/jquery-plugin.js
@@ -1,41 +1,122 @@
-/**
- * jQuery Cookie plugin
- *
- * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
- * Dual licensed under the MIT and GPL licenses:
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- *
- */
-jQuery.cookie = function (key, value, options) {
+// 级联下拉列表
+jQuery.fn.cascade = function(target_or_targets,event) {
+    var OPTIONS_TEMPLATE = "<option value=${value}>${label}</option>";
+    var source = this;
 
-    // key and at least value given, set cookie...
-    if (arguments.length > 1 && String(value) !== "[object Object]") {
-        options = jQuery.extend({}, options);
-
-        if (value === null || value === undefined) {
-            options.expires = -1;
-        }
-
-        if (typeof options.expires === 'number') {
-            var days = options.expires, t = options.expires = new Date();
-            t.setDate(t.getDate() + days);
-        }
-
-        value = String(value);
-
-        return (document.cookie = [
-            encodeURIComponent(key), '=',
-            options.raw ? value : encodeURIComponent(value),
-            options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-            options.path ? '; path=' + options.path : '',
-            options.domain ? '; domain=' + options.domain : '',
-            options.secure ? '; secure' : ''
-        ].join(''));
+    var targets = [];
+    if(jQuery.isArray(target_or_targets)){
+        targets = target_or_targets
+    }else{
+        targets = [String(target_or_targets)]
     }
 
-    // key and possibly options given, get cookie...
-    options = value || {};
-    var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
-    return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
-};
+    function _processEvent(){
+        for(var i=0;i<targets.length;i++){
+            var target = jQuery(targets[i]);
+            // 取得加载数据的链接
+            var href = target.attr("href");
+            var depends = target.attr("depend");
+            if(depends!="")
+                depends=depends.split(",");
+            else
+                depends = [];
+            var url_options = {};
+            var value_length = 0;
+
+            for(var j=0;j<depends.length;j++){
+              if(jQuery("#"+depends[j]).val()&&jQuery("#"+depends[j]).val()!=""){
+                url_options[depends[j]] = jQuery("#"+depends[j]).val();
+                value_length++;
+              }
+            }
+            //decodeURIComponent
+            if(value_length==depends.length||value_length>0){
+                target.html("");
+                if(target.attr("blank")!=""){
+                  option =  jQuery(jQuery.tmpl(OPTIONS_TEMPLATE,{label:target.attr("blank"),value:""})) ;
+                  target.append(option);
+                }
+                href = jQuery.tmpl(decodeURIComponent(href),url_options).text();
+                jQuery.getJSON(href,{},function(datas){
+                    var targetValue = target.attr("origin_value");
+                    datas = datas["items"] ;
+                    if(!datas)
+                      datas = [];
+                    jQuery.each(datas,function(index,data){
+                        var option = jQuery('<option/>');
+                        for(var v in data){
+                            if(v=="label")
+                                option.html(data[v]);
+                            else
+                                option.attr(v,data[v]);
+                        }
+
+                        target.append(option);
+                    });
+
+                    target.val(targetValue);
+                    target.trigger('change');
+                });
+            }else
+            {   target.html("");
+                if(target.attr("blank")!=""){
+                  option =  jQuery(jQuery.tmpl(OPTIONS_TEMPLATE,{label:target.attr("blank"),value:""})) ;
+                  target.append(option);
+                }
+                target.val("");
+                target.trigger("change");
+            }
+        }
+    }
+    jQuery(this).change(_processEvent);
+    return this;
+}
+
+//菜单下拉按钮
+jQuery.fn.menubutton = function(){
+    var menuNode = this;
+    var _menuContent = jQuery(this).find(".menuContent:first");
+    var _menuLabel = jQuery(this).children(".menuLabel:first");
+    _menuLabel.hover(function(){
+        jQuery(this).addClass("menuLabelHover");
+    },function(){
+        jQuery(this).removeClass("menuLabelHover");
+    });
+
+    _menuLabel.click(function(e){
+        if(jQuery(this).hasClass("menuLabelClick")){
+            jQuery(this).removeClass("menuLabelClick");
+            _menuContent.removeClass("menuContentVisual");
+            jQuery(this).removeClass("menuLabelHover");
+            jQuery(document).unbind("click",_globalClickHandler);
+        }else
+        {
+            jQuery(this).addClass("menuLabelClick");
+            _menuContent.addClass("menuContentVisual");
+            jQuery(document).bind("click",_globalClickHandler);
+        }
+    });
+
+    var _globalClickHandler = function(e){
+        if(jQuery(menuNode).find(e.target).length<1){
+            if(_menuLabel.hasClass("menuLabelClick")){
+                _menuLabel.removeClass("menuLabelClick");
+                _menuContent.removeClass("menuContentVisual");
+                _menuLabel.removeClass("menuLabelHover");
+            }
+            jQuery(document).unbind("click",_globalClickHandler);
+        }
+    }
+}
+
+
+//树开菜单
+jQuery.fn.navtree = function(){
+
+}
+
+
+//树开菜单
+jQuery.duelselect = function(){
+
+}
