f579a64101ef486d27806b7272c3ea027c79da58
hailor <hailor1985@gmail.com>
2011-12-01 15:11:46 +0800

    修改事故单自动分派规则,将服务替换为事故单分类

--- app/helpers/icm/support_groups_helper.rb
+++ app/helpers/icm/support_groups_helper.rb
@@ -1,13 +1,15 @@
 module Icm::SupportGroupsHelper
   def support_group_duel_types(exclude=[])
-    duel_types(exclude)+[[Irm::BusinessObject.class_name_to_meaning(Slm::ServiceCatalog.name),Irm::BusinessObject.class_name_to_code(Slm::ServiceCatalog.name)]]
+    duel_types = duel_types(exclude)
+    duel_types << [Irm::BusinessObject.class_name_to_meaning(Icm::IncidentCategory.name),Irm::BusinessObject.class_name_to_code(Icm::IncidentCategory.name)]
+
   end
 
   def support_group_duel_values(exclude=[])
     values = duel_values(exclude)
-    type_code = Irm::BusinessObject.class_name_to_code(Slm::ServiceCatalog.name)
-    type_name = Irm::BusinessObject.class_name_to_meaning(Slm::ServiceCatalog.name)
-    values += Slm::ServiceCatalog.multilingual.enabled.collect{|o| ["#{type_name}:#{o[:name]}","#{type_code}##{o.id}",{:type=>type_code,:query=>o[:name]}]}
+    type_code = Irm::BusinessObject.class_name_to_code(Icm::IncidentCategory.name)
+    type_name = Irm::BusinessObject.class_name_to_meaning(Icm::IncidentCategory.name)
+    values += Icm::IncidentCategory.multilingual.enabled.collect{|o| ["#{type_name}:#{o[:name]}","#{type_code}##{o.id}",{:type=>type_code,:query=>o[:name]}]}
     values
   end
 end
