8a0cdff8ec402b975ac97390a4ba3a74b68fa763
Stanley Liu <stanleyliu@Stanley-LiumatoMacBook-Pro.local>
2011-12-08 15:49:31 +0800

    fixed: 知识库频道LOV，加入可访问约束

--- app/models/skm/channel.rb
+++ app/models/skm/channel.rb
@@ -18,11 +18,7 @@ class Skm::Channel < ActiveRecord::Base
   has_many :groups, :through => :channel_groups
   has_many :entry_headers
 
-  default_scope {default_filter.within_accessible_channels}
-
-  scope :within_accessible_channels, lambda{
-    within_accessible_channels_c
-  }
+  default_scope {default_filter}
 
   scope :without_group, lambda{|group_id|
       where("NOT EXISTS(SELECT * FROM #{Skm::ChannelGroup.table_name} cg WHERE cg.group_id = ? AND cg.opu_id = #{table_name}.opu_id AND cg.channel_id = #{table_name}.id)",
@@ -37,8 +33,8 @@ class Skm::Channel < ActiveRecord::Base
     self.columns.enabled.collect(&:id).join(",")
   end
 
-  def self.within_accessible_channels_c
-    where("EXISTS(SELECT * FROM irm_person_relations_v pr, #{Skm::ChannelGroup.table_name} cgvv WHERE pr.source_type = ? AND pr.person_id = ? AND pr.source_id = cgvv.group_id AND cgvv.channel_id = #{Skm::Channel.table_name}.id)",
+  def self.lov(origin_scope,params)
+    return origin_scope.where("EXISTS(SELECT * FROM irm_person_relations_v pr, #{Skm::ChannelGroup.table_name} cgvv WHERE pr.source_type = ? AND pr.person_id = ? AND pr.source_id = cgvv.group_id AND cgvv.channel_id = #{Skm::Channel.view_name}.id)",
           "IRM__GROUP", Irm::Person.current.id)
   end
 end
