aff48696610514390d2f22e1c7a510bbd0189c4b
hailor <hailor1985@gmail.com>
2011-12-14 23:16:29 +0800

    添加变更单计划类型与变更单计划管理

--- db/data/chm/20111214150436_add_change_journal_plan_function.rb
+++ db/data/chm/20111214150436_add_change_journal_plan_function.rb
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 class AddChangeJournalPlanFunction < ActiveRecord::Migration
   def up
+    chm_change_plan_type= Irm::FunctionGroup.new(:zone_code=>'CHANGE_SETTING',:code=>'CHANGE_PLAN_TYPE',:controller=>'chm/change_plan_types',:action=>'index',:not_auto_mult=>true)
+    chm_change_plan_type.function_groups_tls.build(:language=>'zh',:source_lang=>'en',:name=>'计划类型',:description=>'查看,提交,编辑,操作变更单计划类型')
+    chm_change_plan_type.function_groups_tls.build(:language=>'en',:source_lang=>'en',:name=>'Change Plan Type',:description=>'Submit,edit change plan type')
+    chm_change_plan_type.save
+
+    chm_change_plan_type= Irm::Function.new(:function_group_code=>'CHANGE_PLAN_TYPE',:code=>'CHANGE_PLAN_TYPE',:default_flag=>'N',:login_flag => 'N', :public_flag=>'N',:not_auto_mult=>true)
+    chm_change_plan_type.functions_tls.build(:language=>'zh',:source_lang=>'en',:name=>'计划类型',:description=>'计划类型')
+    chm_change_plan_type.functions_tls.build(:language=>'en',:source_lang=>'en',:name=>'Change Plan Type',:description=>'Change Plan Type')
+    chm_change_plan_type.save
+
+    menu_entiry_109= Irm::MenuEntry.new(:menu_code=>'CHANGE_MANAGEMENT',:sub_menu_code=>nil,:sub_function_group_code=>'CHANGE_PLAN_TYPE',:display_sequence=>50)
+    menu_entiry_109.save
+
     chm_change_journal= Irm::Function.new(:function_group_code=>'CHANGE_REQUEST',:code=>'CHANGE_JOURNAL',:default_flag=>'N',:login_flag => 'N', :public_flag=>'N',:not_auto_mult=>true)
     chm_change_journal.functions_tls.build(:language=>'zh',:source_lang=>'en',:name=>'回复变理单',:description=>'回复变理单')
     chm_change_journal.functions_tls.build(:language=>'en',:source_lang=>'en',:name=>'Reply Change Request',:description=>'Reply Change Request')
