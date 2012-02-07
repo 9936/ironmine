5c3def733bd990bb48357e0e6ae5279adb511960
hailor <hailor1985@gmail.com>
2012-02-07 15:52:00 +0800

    使用Irm::Portlet和Irm::PortletConfig的数据来定义首页portal

--- app/helpers/irm/home_helper.rb
+++ app/helpers/irm/home_helper.rb
@@ -6,4 +6,27 @@ module Irm::HomeHelper
     end
     values
   end
+
+  def portlets_json
+    json_hash = {}
+    Irm::Portlet.multilingual.each do |p|
+      json_hash.merge!({"m#{p.id}"=>{"t"=>p[:name],"url"=>url_for(p.url_options.merge({:wmode=>"portlet"}))}})
+    end
+    json_hash.to_json.html_safe
+  end
+
+  def portlets_config
+    portle_config = Irm::PortletConfig.personal_config(Irm::Person.current.id)
+    if(portle_config.present?&&portle_config[:config].present?)
+      portle_config[:config]
+    else
+      portlets_str = Irm::Portlet.default.collect{|p| "'m#{p.id}:c1'"}.join(",")
+      "{t1:[#{portlets_str}]}"
+    end
+  end
+
+  def portlets_layout
+
+  end
+
 end
