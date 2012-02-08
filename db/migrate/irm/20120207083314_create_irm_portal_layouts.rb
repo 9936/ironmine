1fa65074620c58586488ed6bb80c30b3a20e6005
chenmaolin <maolin.chen@hand-china.com>
2012-02-08 14:34:02 +0800

    修正创建portal布局表的脚本

--- db/migrate/irm/20120207083314_create_irm_portal_layouts.rb
+++ db/migrate/irm/20120207083314_create_irm_portal_layouts.rb
@@ -32,7 +32,7 @@ class CreateIrmPortalLayouts < ActiveRecord::Migration
     add_index "irm_port_layouts_tl", ["layout_id"],             :name => "IRM_PORT_LAYOUTS_TL_N1"
     execute('CREATE OR REPLACE VIEW irm_port_layouts_vl AS SELECT t.*,tl.id lang_id,tl.name,tl.description,tl.language,tl.source_lang
                          FROM irm_port_layouts t,irm_port_layouts_tl tl
-                         WHERE t.id = tl.layout_id')
+                         WHERE t.id = tl.portal_layout_id')
   end
 
   def down
