5ccfa29b0e527655bce99577f58c742a0dd8ac3f
chenmaolin <maolin.chen@hand-china.com>
2012-02-08 13:41:13 +0800

    为portal_layout生成scaffold

---
+++ app/helpers/irm/portal_layouts_helper.rb
@@ -0,0 +1,3 @@
+module Irm::PortalLayoutsHelper
+
+end
---
+++ app/models/irm/portal_layout.rb
@@ -0,0 +1,24 @@
+class Irm::PortalLayout < ActiveRecord::Base
+  set_table_name :irm_port_layouts
+  after_save :process_default
+  #多语言关系
+  attr_accessor :name, :description
+  has_many   :portal_layouts_tls,:dependent => :destroy
+  acts_as_multilingual
+
+  validates_presence_of  :layout ,:default_flag
+
+  validates_uniqueness_of :layout,:scope=>[:opu_id], :if => Proc.new { |i| !i.layout.blank? }
+  validates_format_of :layout, :with => /^[0-9]+,[0-9]+,[0-9]+$/ ,:if=>Proc.new{|i| !i.layout.blank?},:message=>:label_irm_portal_layout_format_error
+
+   #加入activerecord的通用方法和scope
+  query_extend
+  # 对运维中心数据进行隔离
+  default_scope {default_filter}
+
+  private
+  def process_default
+    return true unless self.default_flag.eql?(Irm::Constant::SYS_YES)
+    self.class.where("default_flag = ? AND id != ?", Irm::Constant::SYS_YES,self.id).update_all(:default_flag=>Irm::Constant::SYS_NO)
+  end
+end
