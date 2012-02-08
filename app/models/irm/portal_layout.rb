7e6e57e37c30f19ca54c000b6e5bfee5fad5c9dc
chenmaolin <maolin.chen@hand-china.com>
2012-02-08 15:45:02 +0800

    调整布局格式的要求，增加处理中文逗号的代码

--- app/models/irm/portal_layout.rb
+++ app/models/irm/portal_layout.rb
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 class Irm::PortalLayout < ActiveRecord::Base
   set_table_name :irm_port_layouts
-  after_save :process_default
+  after_save :process_default ,:process_comma
   #多语言关系
   attr_accessor :name, :description
   has_many   :portal_layouts_tls,:dependent => :destroy
@@ -9,7 +10,7 @@ class Irm::PortalLayout < ActiveRecord::Base
   validates_presence_of  :layout ,:default_flag
 
   validates_uniqueness_of :layout,:scope=>[:opu_id], :if => Proc.new { |i| !i.layout.blank? }
-  validates_format_of :layout, :with => /^[0-9]+,[0-9]+,[0-9]+$/ ,:if=>Proc.new{|i| !i.layout.blank?},:message=>:label_irm_portal_layout_format_error
+  validates_format_of :layout, :with => /^[12]([,，][12])*$/ ,:if=>Proc.new{|i| !i.layout.blank?},:message=>:label_irm_portal_layout_format_error
 
    #加入activerecord的通用方法和scope
   query_extend
@@ -20,5 +21,10 @@ class Irm::PortalLayout < ActiveRecord::Base
   def process_default
     return true unless self.default_flag.eql?(Irm::Constant::SYS_YES)
     self.class.where("default_flag = ? AND id != ?", Irm::Constant::SYS_YES,self.id).update_all(:default_flag=>Irm::Constant::SYS_NO)
+  end
+  private
+  def process_comma
+    return true unless self.layout.count("，") > 0
+    self.class.where("id = ?", self.id).update_all(:layout=>self.layout.gsub(/，/,','))
   end
 end
