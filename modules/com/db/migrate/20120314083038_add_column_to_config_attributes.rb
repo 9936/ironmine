170e5418fc78104f1c4c14a2eece93207728a960
uudui <442453867@qq.com>
2012-03-14 16:46:08 +0800

    添加配置分类和配置属性设置功能

--- modules/com/db/migrate/20120314083038_add_column_to_config_attributes.rb
+++ modules/com/db/migrate/20120314083038_add_column_to_config_attributes.rb
@@ -2,7 +2,7 @@ class AddColumnToConfigAttributes < ActiveRecord::Migration
   def up
     add_column :com_config_attributes, "input_value", :string,:limit=>200
     add_column :com_config_attributes, "regular", :string,:limit=>100
-    add_column :com_config_attributes, "required_flag",  :limit => 1, :null => false,:default=>"N"
+    add_column :com_config_attributes, "required_flag", :string,limit => 1, :null => false,:default=>"N"
   end
   def down
     add_column :com_config_attributes, "input_value"
