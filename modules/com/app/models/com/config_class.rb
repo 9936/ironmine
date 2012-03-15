f4925d42502893ed764d9f23877a61f676542321
uudui <442453867@qq.com>
2012-03-15 13:10:46 +0800

    新增配置分类子分类和父级分类关系功能

--- modules/com/app/models/com/config_class.rb
+++ modules/com/app/models/com/config_class.rb
@@ -1,8 +1,10 @@
 class Com::ConfigClass < ActiveRecord::Base
   set_table_name :com_config_classes
+  after_save :explore_config_class_hierarchy
 
+  attr_accessor :level
   #多语言关系
-  attr_accessor :name,:description,:level
+  attr_accessor :name,:description
   has_many :config_classes_tls,:dependent => :destroy
   acts_as_multilingual
 
@@ -12,5 +14,9 @@ class Com::ConfigClass < ActiveRecord::Base
   query_extend
   #对运维中心数据进行隔离
   default_scope {default_filter}
+
+  def explore_config_class_hierarchy
+    Com::ConfigClassExplosion.explore_hierarchy(self.id,self.parent_id)
+  end
 
 end
--- modules/com/lib/com/init_permission.rb
+++ modules/com/lib/com/init_permission.rb
@@ -5,18 +5,11 @@ Irm::AccessControl.map do |map|
 
   }
 
-  #===================com/config_classes============================
-  #["index", "new", "edit", "create", "update", "show", "get_data", "destroy", "multilingual_edit", "multilingual_update"]
-  map.function :login_function,{"com/config_classes"=>["index", "show","get_data"]}
-  map.function :login_function,{"com/config_classes"=>["new", "create"]}
-  map.function :login_function,{"com/config_classes"=>["edit", "update", "multilingual_edit", "multilingual_update"]}
+  map.function :config_class,{
+      "com/config_classes"=>["index", "show","get_data", "new", "create", "edit", "update", "multilingual_edit", "multilingual_update"],
+      "com/config_attributes"=>["index", "show","get_data","new", "create","edit", "update", "multilingual_edit", "multilingual_update"]
+  }
 
-
-  #===================com/config_attributes============================
-  #["index", "new", "edit", "create", "update", "show", "get_data", "destroy", "multilingual_edit", "multilingual_update"]
-  map.function :login_function,{"com/config_attributes"=>["index", "show","get_data"]}
-  map.function :login_function,{"com/config_attributes"=>["new", "create"]}
-  map.function :login_function,{"com/config_attributes"=>["edit", "update", "multilingual_edit", "multilingual_update"]}
 
 
 end
