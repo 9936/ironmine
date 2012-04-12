5b8d1095351650dbde18c729ac1bf9f39c1c3b98
uudui <442453867@qq.com>
2012-04-12 16:16:19 +0800

    为bootstrap版本表格添加添加功能： 1、配置排序列 2、配置select => single or multiple 3、选择每页显示记录数

--- modules/bootstrap/app/assets/javascripts/locales/jquery-en.js
+++ modules/bootstrap/app/assets/javascripts/locales/jquery-en.js
@@ -5,7 +5,13 @@ var irm_labels = {
     paginatorNext: "Next",
     paginatorBeforePage: "Page",
     paginatorAfterPage: "/Total ${totalPage}",
-    paginatorRecord: "Record ${start}-${end}/${totalCount}",
+    paginatorRecord: "Current Records From ${start}-${end}/${totalCount}",
+    paginatorBeforeSize: "Each Page Show",
+    paginatorAfterSize: "Records, ",
     paginatorRefresh: "Refresh",
+    image_from_clipboard: "Image From Clipboard",
+    sort_this_column: "Click Here To Order This Column",
+    select_all:"Click Here To Select All",
+    select_single: "Click Here To Select Current Item",
     search: "Search"
 };
