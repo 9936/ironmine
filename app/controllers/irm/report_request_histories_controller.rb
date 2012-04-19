256a88a8d86e158a3012e2517d6f72605c4e4f4e
uudui <442453867@qq.com>
2012-04-19 17:16:12 +0800

    监控==>报表运行历史 所有页面bootstrap样式

--- app/controllers/irm/report_request_histories_controller.rb
+++ app/controllers/irm/report_request_histories_controller.rb
@@ -8,15 +8,13 @@ class Irm::ReportRequestHistoriesController < ApplicationController
 
   def get_data
     histories_scope = Irm::ReportRequestHistory.list_all.order("#{Irm::ReportRequestHistory.table_name}.created_at desc")
-
+    histories_scope,count = paginate(histories_scope)
     respond_to do |format|
-      format.json {
-        histories_scope,count = paginate(histories_scope)
-        render :json=>to_jsonp(histories_scope.to_grid_json([:report_name,
-                                                             :executed_name,
-                                                           :created_at,
-                                                           :execute_type,
-                                                           :start_at,:end_at,:elapse],count))}
+      format.json {render :json=>to_jsonp(histories_scope.to_grid_json([:report_name,:executed_name,:created_at,:execute_type,:start_at,:end_at,:elapse],count))}
+      format.html {
+        @count = count
+        @datas = histories_scope
+      }
     end
   end
 
