ec5b9a5b83c647ab4cf1e16731122b6bd448e923
stanleyliu <ivanmarkliu@gmail.com>
2012-04-20 09:39:59 +0800

    报表：被升级事故单比例（%） 描述： 统计选定的时间范围内，提交到服务台，并出现升级操作的事故单总数，与总提交数量的比例 参数： 日期：月 应用系统 列：出现升级的事故单总数，提交的事故单总数，比例 行：应用系统

---
+++ program/report/icm_upgrade_rate.rb
@@ -0,0 +1,72 @@
+class IcmUpgradeRate < Irm::ReportManager::ReportBase
+  def data(params={})
+    params||={}
+
+    incident_request_table = Icm::IncidentRequest.table_name
+
+    statis = Icm::IncidentRequest.
+        select("#{incident_request_table}.external_system_id, count(1) amount").
+        group("#{incident_request_table}.external_system_id")
+
+    external_systems = Irm::ExternalSystem.multilingual
+
+    if params[:external_system_id].present? &&
+        params[:external_system_id].size > 0 &&
+        params[:external_system_id][0].present?
+      statis = statis.where("external_system_id IN (?)", params[:external_system_id] + [])
+      external_systems = external_systems.where("#{Irm::ExternalSystem.table_name}.id IN (?)", params[:external_system_id] + [])
+    end
+
+    datas = []
+    headers = [I18n.t(:label_date),
+               I18n.t(:label_icm_upgrade_amount),
+               I18n.t(:label_total_amount),
+               I18n.t(:label_icm_upgrade_rate)]
+
+    external_systems.each do |e|
+      data = Array.new(4)
+      data[0] = e[:system_name]
+      rec = statis.
+          where("#{incident_request_table}.external_system_id = ?", e.id).
+          where(%Q(date_format(#{incident_request_table}.submitted_date, '%Y-%m') = ? OR
+            EXISTS (SELECT * FROM icm_incident_journals ir
+                      WHERE ir.incident_request_id = #{incident_request_table}.id AND ir.reply_type = 'UPGRADE'
+                      AND date_format(ir.created_at, '%Y-%m') = ?)),
+            Date.strptime("#{params[:year]}-#{params[:month]}", '%Y-%m').strftime("%Y-%m"),
+            Date.strptime("#{params[:year]}-#{params[:month]}", '%Y-%m').strftime("%Y-%m"))
+      upgrade_rec = statis.
+                where("#{incident_request_table}.external_system_id = ?", e.id).
+                where(%Q(EXISTS (SELECT * FROM icm_incident_journals ir
+                            WHERE ir.incident_request_id = #{incident_request_table}.id AND ir.reply_type = 'UPGRADE'
+                            AND date_format(ir.created_at, '%Y-%m') = ?)),
+                  Date.strptime("#{params[:year]}-#{params[:month]}", '%Y-%m').strftime("%Y-%m"))
+      data[1] = upgrade_rec.any? ? upgrade_rec.first[:amount]  : 0
+      data[2] = rec.any? ? rec.first[:amount]  : 0
+      percent = data[2] == 0 ? 0.to_f : ((data[1] / data[2].to_f * 100 * 100).round / 100.0).to_f
+      if percent == 0.to_f
+        percent = 0
+      end
+      data[3] = percent.to_s + "%"
+      datas << data
+    end
+
+    {:datas=>datas,:headers=>headers,:params=>params}
+  end
+
+  def to_xls(params)
+    columns = []
+
+    result = data(params)
+
+    result[:headers].each_with_index do |sh,index|
+      columns << {:key=>index.to_s.to_sym,:label=>sh}
+    end
+
+    excel_data = []
+    result[:datas].each_with_index do |data,index|
+      excel_data << data.to_cus_hash
+    end
+
+    excel_data.to_xls(columns,{})
+  end
+end
