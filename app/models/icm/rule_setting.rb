72b4ef56bbf899cb4eccedcd842996a5a604ae5b
uudui <442453867@qq.com>
2012-04-23 00:06:51 +0800

    修复页面中的一些Bugs

--- app/models/icm/rule_setting.rb
+++ app/models/icm/rule_setting.rb
@@ -2,6 +2,7 @@ class Icm::RuleSetting < ActiveRecord::Base
   set_table_name :icm_rule_settings
 
 
+  validates_presence_of :auto_closure_days
   #加入activerecord的通用方法和scope
   query_extend
   # 对运维中心数据进行隔离
