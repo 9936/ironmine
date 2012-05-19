872f8b2729df390268be4ba2f57de21347e644da
hailor <hailor1985@gmail.com>
2012-05-19 22:53:24 +0800

    修改系统错误页面,添加保存错误消息到本地功能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,6 @@ class ApplicationController < ActionController::Base
 
   # 检查是否需要登录
   def check_if_login_required
-    raise "sdfsafas"
     # 如果用户已经登录,则无需登录,否则转向登录页面
     if Irm::Person.current.logged?||public_permission?
       return true
