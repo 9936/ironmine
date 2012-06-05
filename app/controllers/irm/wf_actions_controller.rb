13f98009f7d7d5c258c8d8394b45f1dfee8cebc7
hailor <hailor1985@gmail.com>
2012-06-05 17:35:58 +0800

    修正工作流规则动作

--- app/controllers/irm/wf_actions_controller.rb
+++ app/controllers/irm/wf_actions_controller.rb
@@ -16,9 +16,9 @@ class Irm::WfActionsController < ApplicationController
     return unless source_str.present?
     source_info = source_str.split(",")
     if source_info.size>1&&"WF_RULE".eql?(source_info[0])
-      Irm::WfRuleAction.create(:rule_id=>source_info[1],:action_type=>action.class,:action_reference_id=>action.id)
+      Irm::WfRuleAction.create(:rule_id=>source_info[1],:action_type=>action.class.name,:action_reference_id=>action.id)
     elsif source_info.size>2&&"WF_RULE_TIME_TRIGGER".eql?(source_info[0])
-      Irm::WfRuleAction.create(:rule_id=>source_info[1],:time_trigger_id=>source_info[2],:action_type=>action.class,:action_reference_id=>action.id)
+      Irm::WfRuleAction.create(:rule_id=>source_info[1],:time_trigger_id=>source_info[2],:action_type=>action.class.name,:action_reference_id=>action.id)
     elsif source_info.size>1&&["AP_SUBMIT","AP_RECALL","AP_FINAL_REJECT","AP_FINAL_APPROVAL"].include?(source_info[0])
       Irm::WfApprovalAction.create(:action_mode=>source_info[0],:action_type=>action.class.name,:action_id=>action.id,:process_id=>source_info[1])
     elsif source_info.size>2&&["AP_STEP_APPROVAL","AP_STEP_REJECT"].include?(source_info[0])
