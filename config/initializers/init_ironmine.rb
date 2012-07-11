01e4794db18cf603755a3374d229089a79a0d009
uudui <442453867@qq.com>
2012-07-11 15:11:57 +0800

    修正在正式环境中：uninitialized constant Sunspot::SessionProxy::DelayedJobSessionProxy Bug

--- config/initializers/init_ironmine.rb
+++ config/initializers/init_ironmine.rb
@@ -138,4 +138,5 @@ end
 #sunspot_plus
 # set the session to the delayed_job handler - this will send all model CRUD reindexing to delayed_job
 require 'sunspot_rails'
+require 'sunspot_plus/session_proxy/delayed_job_session_proxy'
 Sunspot.session = Sunspot::SessionProxy::DelayedJobSessionProxy.new(Sunspot.session)
