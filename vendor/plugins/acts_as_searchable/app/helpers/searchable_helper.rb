36e58f170160b4a8c0a38fa6fdf06797d1f65148
uudui <442453867@qq.com>
2012-07-11 15:36:18 +0800

    修正搜索结果中无数据记录提示信息Bug

--- vendor/plugins/acts_as_searchable/app/helpers/searchable_helper.rb
+++ vendor/plugins/acts_as_searchable/app/helpers/searchable_helper.rb
@@ -2,6 +2,7 @@ module SearchableHelper
   def show_search_result(query, search_option_str = "")
     result_box = ""
     results = []
+    not_found = true
     return unless query.present?
     Ironmine::Acts::Searchable.searchable_entity.each do |key,value|
       next unless !value.present?||allow_to_function?(value)
@@ -10,11 +11,12 @@ module SearchableHelper
       if search_entity.searchable_options[:all].present?&&search_entity.respond_to?(search_entity.searchable_options[:all].to_sym)
         results =  search_entity.send(search_entity.searchable_options[:all].to_sym,query)
         if results.any?
+          not_found = false if not_found
           result_box << render(:partial=>search_entity.searchable_options[:view],:locals=>{:results=>results} )
         end
       end
     end
-    unless results.any?
+    if not_found
       result_box << "<h2>Your search returned no matches.</h2>"
     end
     result_box.html_safe
