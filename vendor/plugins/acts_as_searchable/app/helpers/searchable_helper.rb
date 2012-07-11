f2968decaafaf73d440c22ce194d2d69916486a4
uudui <442453867@qq.com>
2012-07-11 10:25:09 +0800

    修改系统搜索结果表格样式，当没有搜索记录时添加相应的提示信息

--- vendor/plugins/acts_as_searchable/app/helpers/searchable_helper.rb
+++ vendor/plugins/acts_as_searchable/app/helpers/searchable_helper.rb
@@ -5,15 +5,17 @@ module SearchableHelper
     return unless query.present?
     Ironmine::Acts::Searchable.searchable_entity.each do |key,value|
       next unless !value.present?||allow_to_function?(value)
-      next unless !search_option_str.present? || (search_option_str.present? &&
-          (search_option_str.split(" ").include?("ALL") || search_option_str.split(" ").include?(key)))
+      next unless !search_option_str.present? || (search_option_str.present? && (search_option_str.split(" ").include?("ALL") || search_option_str.split(" ").include?(key)))
       search_entity = key.constantize
       if search_entity.searchable_options[:all].present?&&search_entity.respond_to?(search_entity.searchable_options[:all].to_sym)
         results =  search_entity.send(search_entity.searchable_options[:all].to_sym,query)
         if results.any?
-          result_box<<render(:partial=>search_entity.searchable_options[:view],:locals=>{:results=>results} )
+          result_box << render(:partial=>search_entity.searchable_options[:view],:locals=>{:results=>results} )
         end
       end
+    end
+    unless results.any?
+      result_box << "<h2>Your search returned no matches.</h2>"
     end
     result_box.html_safe
   end
