8c030778c27d36bf830f6669a6a3c35f5b9fe29f
hailor <hailor1985@gmail.com>
2012-07-12 15:11:02 +0800

    使用gollum实现wiki功能

--- lib/irm/init_permission.rb
+++ lib/irm/init_permission.rb
@@ -337,7 +337,7 @@ Irm::AccessControl.map do |map|
   map.function :rating_config,{"irm/rating_configs"=>["index", "edit", "update","get_data", "new", "create", "show"]}
   #====================irm/wikis===============================
   map.function :view_wiki,{"irm/wikis" => ["index","get_data","show","compare","history"]}
-  map.function :create_wiki,{"irm/wikis" => ["new","create"]}
-  map.function :edit_wiki,{"irm/wikis" => ["edit","update"]}
+  map.function :create_wiki,{"irm/wikis" => ["new","create","preview"]}
+  map.function :edit_wiki,{"irm/wikis" => ["edit","update","preview"]}
   map.function :manage_wiki,{"irm/wikis" => ["destroy","revert"]}
 end
