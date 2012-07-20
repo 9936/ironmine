e0f8c9a4abe4d4ffd9b54ec43d8eba5db7209743
uudui <442453867@qq.com>
2012-07-20 11:27:01 +0800

    1、引入附件索引相关的jar包； 2、修改solr的配置文件； 3、增加sunspot_cell插件。

--- app/models/irm/attachment.rb
+++ app/models/irm/attachment.rb
@@ -5,7 +5,7 @@ class Irm::Attachment < ActiveRecord::Base
 
   has_many :attachment_versions, :dependent => :destroy
 
-    #加入activerecord的通用方法和scope
+  #加入activerecord的通用方法和scope
   query_extend
   # 对运维中心数据进行隔离
   default_scope {default_filter}
