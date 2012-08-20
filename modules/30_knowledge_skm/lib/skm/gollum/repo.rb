f7268e94ae104ab1c48c979755b4d6101ef368ca
hailor <hailor1985@gmail.com>
2012-08-20 17:39:40 +0800

    修正wiki中文文件名无法进行版本对比的bug

--- modules/30_knowledge_skm/lib/skm/gollum/repo.rb
+++ modules/30_knowledge_skm/lib/skm/gollum/repo.rb
@@ -1,13 +1,18 @@
 module Skm::Gollum::Repo
-  def diff(a, b, *paths)
-    diff = self.git.native('diff', {}, a, b, '--', *paths)
-    # 修正中文文件名无法进行diff的bug
-    diff.gsub!("diff --git \"a/","diff --git a\"")
-    if diff =~ /diff --git a/
-      diff = diff.sub(/.*?(diff --git a)/m, '\1')
-    else
-      diff = ''
+  def self.included(base)
+    base.class_eval do
+      def diff(a, b, *paths)
+        diff = self.git.native('diff', {}, a, b, '--', *paths)
+        # 修正中文文件名无法进行diff的bug
+        if diff =~ /diff --git a/
+          diff = diff.sub(/.*?(diff --git a)/m, '\1')
+        elsif diff =~ /diff --git "a/
+          diff = diff.sub(/.*?(diff --git "a)/m, '\1')
+        else
+          diff = ''
+        end
+        Diff.list_from_string(self, diff)
+      end
     end
-    Diff.list_from_string(self, diff)
   end
 end
