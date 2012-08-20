9d7175ca7bd7afe5334fc93ae01f706a26287e2f
hailor <hailor1985@gmail.com>
2012-08-20 16:43:23 +0800

    修正wiki中文文件名无法进行版本对比的bug

--- modules/30_knowledge_skm/config/initializers/init_wiki.rb
+++ modules/30_knowledge_skm/config/initializers/init_wiki.rb
@@ -1,6 +1,7 @@
 module Ironmine
   Gollum::Markup.send(:include,Skm::Gollum::Markup)
   Gollum::Page.send(:include,Skm::Gollum::Page)
+  Grit::Repo.send(:include,Skm::Gollum::Repo)
   WIKI = Gollum::Wiki.new(Rails.root.join("..","wiki", ".git"))
 
 end
