e1706c17ae99b8d6a7e97a115b79113190c70ace
uudui <442453867@qq.com>
2012-08-21 20:48:04 +0800

    修复在IE6 下的bugs

--- modules/80_bootstrap_bsp/app/assets/javascripts/ie6.js
+++ modules/80_bootstrap_bsp/app/assets/javascripts/ie6.js
@@ -1,8 +1,47 @@
+//(function($) {
+//    $.fn.outerHTML = function(s) {
+//        return (s)
+//            ? this.before(s).remove() : $('<p>').append(this.eq(0).clone()).html();
+//    }
+//})(jQuery);
+
 $(function () {
     $('.row div[class^="span"]:last-child').addClass("last-child");
     $('[class="span"]').addClass("margin-left-20");
     $(':button[class="btn"], :reset[class="btn"], :submit[class="btn"], input[type="button"]').addClass("button-reset");
     $(":checkbox").addClass("input-checkbox");
     $('[class^="icon-"], [class=" icon-"]').addClass("icon-sprite");
+    //为input[type="text"]
+    $('input[type="text"]').addClass("input-text");
+    $(".lov-btn").each(function(){
+       $(this).addClass("add-on");
+       $(this).text("查找");
+    });
+    //修复IE6下select样式无法生效
+//    $("select").each(function(){
+//       $(this).css("margin", "-2px").css("overflow","hidden");
+//       var w = ($(this).width() -3) + 'px';
+//       alert($(this).outerHTML());
+//       var select_box = $("<div>"+$(this).outerHTML()+"</div>");
+//       select_box.css("width",w).css("position","relative").css("height","20px").css("border","1px solid #ccc");
+//       $(this).parent().append(select_box);
+//       $(this).remove();
+//    });
 });
 
+//处理IE中的select
+function select_for_ie6(show){
+    //隐藏select
+    if(show){
+        $("select").each(function(){
+            $(this).attr("show_select",true);
+            $(this).hide();
+        })
+    }else{
+        $("select").each(function(){
+            if($(this).attr("show_select")){
+              $(this).show();
+            }
+        })
+    }
+}
