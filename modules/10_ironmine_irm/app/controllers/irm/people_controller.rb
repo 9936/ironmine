932ebf573bfdacd20d07d333d4719b6a238e0644
stanleyliu <ivanmarkliu@gmail.com>
2012-08-27 14:52:36 +0800

    fixed: 修正人员查看页面，两个表格的翻页栏不显示的问题

--- modules/10_ironmine_irm/app/controllers/irm/people_controller.rb
+++ modules/10_ironmine_irm/app/controllers/irm/people_controller.rb
@@ -198,13 +198,15 @@ class Irm::PeopleController < ApplicationController
   def get_owned_external_systems
     @person = Irm::Person.find(params[:person_id])
     systems_scope = @person.external_systems
+    systems_scope = @person.external_systems
+    systems_scope = systems_scope.match_value("#{Irm::ExternalSystemsTl.table_name}.system_name",params[:system_name])
     systems, count = paginate(systems_scope)
     respond_to do |format|
       format.html  {
         @datas = systems
         @count = count
       }
-      format.json {render :json => to_jsonp(systems.to_grid_json([:system_name, :system_description, :external_system_code, :status_code], count))}
+      #format.json {render :json => to_jsonp(systems.to_grid_json([:system_name, :system_description, :external_system_code, :status_code], count))}
     end
   end
 
