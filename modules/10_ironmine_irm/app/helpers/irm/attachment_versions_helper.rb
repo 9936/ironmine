ea932acbf9a967cda7832c9a73c408ec3926ba69
uudui <442453867@qq.com>
2012-09-28 17:57:03 +0800

    获取附件类型添加按sequence排序

--- modules/10_ironmine_irm/app/helpers/irm/attachment_versions_helper.rb
+++ modules/10_ironmine_irm/app/helpers/irm/attachment_versions_helper.rb
@@ -8,7 +8,7 @@ module Irm::AttachmentVersionsHelper
   end
 
   def available_file_categories
-    Irm::LookupValue.multilingual.query_by_lookup_type("SKM_FILE_CATEGORIES").order_id.collect{|p| [p[:meaning], p.id]}
+    Irm::LookupValue.multilingual.query_by_lookup_type("SKM_FILE_CATEGORIES").order_by_sequence.order_id.collect{|p| [p[:meaning], p.id]}
   end
 
 end
