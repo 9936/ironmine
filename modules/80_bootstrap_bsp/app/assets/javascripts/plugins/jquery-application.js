6a6910d5ad6ab45aa3b0c5fd865297bc7d02f599
uudui <442453867@qq.com>
2012-10-09 16:08:49 +0800

    添加功能函数将文件大小转换为带有文件大小单位（GB,M,KB）的值

--- modules/80_bootstrap_bsp/app/assets/javascripts/plugins/jquery-application.js
+++ modules/80_bootstrap_bsp/app/assets/javascripts/plugins/jquery-application.js
@@ -528,3 +528,18 @@ var scrollToTop = scrollToTop || {
     }
 };
 //END =================================返回顶部================================
+
+//START =================================根据bytes值返回文件大小================================
+function formatFileSize(bytes){
+    if (typeof bytes !== 'number') {
+        return '';
+    }
+    if (bytes >= 1000000000) {
+        return (bytes / 1000000000).toFixed(2) + ' GB';
+    }
+    if (bytes >= 1000000) {
+        return (bytes / 1000000).toFixed(2) + ' MB';
+    }
+    return (bytes / 1000).toFixed(2) + ' KB';
+}
+//END =================================根据bytes值返回文件大小================================
