e38e7d5daf5289077f60065f68ee7964b958db0e
hailor <hailor1985@gmail.com>
2012-10-17 16:20:33 +0800

    修改LDAP同步用户的系统,组无法删除的问题

--- modules/10_ironmine_irm/db/migrate/20121017065456_repair_ldap_opu_id.rb
+++ modules/10_ironmine_irm/db/migrate/20121017065456_repair_ldap_opu_id.rb
@@ -1,9 +1,6 @@
 class RepairLdapOpuId < ActiveRecord::Migration
   def up
-    Irm::ExternalSystemPerson.each do |esp|
-      esp.update_attributes(:opu_id=>Irm::Person.find(esp.person_id).opu_id)
-    end
-    Irm::ExternalSystemPerson.each do |esp|
+    Irm::ExternalSystemPerson.all.each do |esp|
       esp.update_attributes(:opu_id=>Irm::Person.find(esp.person_id).opu_id)
     end
   end
