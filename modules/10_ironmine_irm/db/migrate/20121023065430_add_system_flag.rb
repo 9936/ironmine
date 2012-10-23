42e5f3ec2c6264bed59cd13d5fc132bef43376a0
uudui <442453867@qq.com>
2012-10-23 15:50:04 +0800

    给permission,function,function_group添加system_flag，并更新相应的视图

---
+++ modules/10_ironmine_irm/db/migrate/20121023065430_add_system_flag.rb
@@ -0,0 +1,27 @@
+class AddSystemFlag < ActiveRecord::Migration
+  def up
+    add_column :irm_permissions, :system_flag, :string, :limit => 1, :default => 'N', :after => 'code', :null => false
+    add_column :irm_functions, :system_flag, :string, :limit => 1, :default => 'N', :after => 'code', :null => false
+    add_column :irm_function_groups, :system_flag, :string, :limit => 1, :default => 'N', :after => 'code', :null => false
+
+    #更新视图
+    execute('CREATE OR REPLACE VIEW irm_functions_vl AS SELECT t.*,tl.id lang_id,tl.name,tl.description,tl.language,tl.source_lang
+             FROM irm_functions  t,irm_functions_tl tl WHERE t.id = tl.function_id')
+
+    execute('CREATE OR REPLACE VIEW irm_function_groups_vl AS SELECT t.*,tl.id lang_id,tl.name,tl.description,tl.language,tl.source_lang
+             FROM irm_function_groups t,irm_function_groups_tl tl WHERE t.id = tl.function_group_id')
+  end
+
+  def down
+    remove_column :irm_permissions, :system_flag
+    remove_column :irm_functions, :system_flag
+    remove_column :irm_function_groups, :system_flag
+
+    #更新视图
+    execute('CREATE OR REPLACE VIEW irm_functions_vl AS SELECT t.*,tl.id lang_id,tl.name,tl.description,tl.language,tl.source_lang
+             FROM irm_functions  t,irm_functions_tl tl WHERE t.id = tl.function_id')
+
+    execute('CREATE OR REPLACE VIEW irm_function_groups_vl AS SELECT t.*,tl.id lang_id,tl.name,tl.description,tl.language,tl.source_lang
+             FROM irm_function_groups t,irm_function_groups_tl tl WHERE t.id = tl.function_group_id')
+  end
+end
