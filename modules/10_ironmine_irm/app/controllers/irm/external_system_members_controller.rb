55d567a9ec1a5be83f85187bebd05cb5c36fcb4a
uudui <442453867@qq.com>
2012-10-29 13:32:16 +0800

    系统也有成员中显示成员的系统简单信息

--- modules/10_ironmine_irm/app/controllers/irm/external_system_members_controller.rb
+++ modules/10_ironmine_irm/app/controllers/irm/external_system_members_controller.rb
@@ -23,9 +23,7 @@ class Irm::ExternalSystemMembersController < ApplicationController
   end
 
   def get_owned_members_data
-    member_scope = Irm::Person.
-                      with_organization(I18n.locale).
-                      with_external_system(params[:external_system_id])
+    member_scope = Irm::Person.with_organization(I18n.locale).with_external_system(params[:external_system_id])
     member_scope = member_scope.match_value("#{Irm::Organization.view_name}.name", params[:organization_name])
     member_scope = member_scope.match_value("#{Irm::Person.table_name}.full_name",params[:full_name])
     member_scope = member_scope.match_value("#{Irm::Person.table_name}.email_address",params[:email_address])
