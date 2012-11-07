bdba7950b81858eb750d8862e26314eed4dbebe1
uudui <442453867@qq.com>
2012-11-07 16:28:21 +0800

    修正多个应用下session cookies冲突

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,5 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-Ironmine::Application.config.session_store :active_record_store
+#Ironmine::Application.config.session_store :active_record_store
+Ironmine::Application.config.session_store :cookie_store, :key => '_new_ironmine_session_cookie'
