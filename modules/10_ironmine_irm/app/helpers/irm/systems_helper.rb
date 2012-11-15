fa7b035fe36bc9466baa88ac53fb21a64f5621b4
uudui <442453867@qq.com>
2012-11-15 17:59:00 +0800

    将系统设置的layout设置为setting.html.erb

--- modules/10_ironmine_irm/app/helpers/irm/systems_helper.rb
+++ modules/10_ironmine_irm/app/helpers/irm/systems_helper.rb
@@ -1,17 +1,20 @@
 module Irm::SystemsHelper
   def current_system_menu
     return nil unless Irm::Person.current&&Irm::Person.current.profile
+    return nil unless params[:sid]
     systems = Irm::Person.current.external_systems
     return nil unless systems.size>1
     system = ""
 
     system << <<-BEGIN_HEML
+    <ul class="nav nav-pills  pull-right" style="text-align: left;">
       <li class="dropdown">
         <a href="#" data-toggle="dropdown" class="dropdown-toggle">#{current_system[:system_name]} <b class="caret"></b></a>
         <ul class="dropdown-menu" style="min-width:130px;right:0;left:auto;" >
           #{list_systems(systems)}
         </ul>
       </li>
+    </ul>
     BEGIN_HEML
 
     system.html_safe
@@ -29,10 +32,11 @@ module Irm::SystemsHelper
 
   # 生成系统菜单
   def list_systems(systems)
-    puts "#{systems.to_json}"
     links = ""
     systems.each do |s|
-      links << content_tag(:li,link_to(s[:system_name],{:controller => "irm/systems", :action => "index", :sid => s.id}),{:class=>"menuItem"})
+      menu = Irm::MenuManager.sub_entries_by_menu(Irm::Menu.system_menu.id,true,s.id).first
+      links << content_tag(:li,link_to(s[:system_name],menu[:url_options].merge({:controller=>menu[:controller],:action=>menu[:action],:mi=>menu[:menu_entry_id],:level=>1})),{}) if menu
+      #links << content_tag(:li,link_to(s[:system_name],{:controller => "irm/systems", :action => "index", :sid => s.id}),{:class=>"menuItem"})
     end
 
     links.html_safe
