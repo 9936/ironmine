688cfc074ae07d36bd35d1d4d18c4951e1fc10f8
uudui <442453867@qq.com>
2012-12-11 16:06:39 +0800

    优化自定义字段获取自定义属性逻辑代码

--- modules/10_ironmine_irm/lib/irm/custom_fields.rb
+++ modules/10_ironmine_irm/lib/irm/custom_fields.rb
@@ -9,39 +9,21 @@ module Irm::CustomFields
       return if self.included_modules.include?(::Irm::CustomFields::InstanceMethods)
       send :include, ::Irm::CustomFields::InstanceMethods
 
-      if options[:system_flag] == 'Y'
-        class_eval do
-          def custom_attributes
-            #取得对应model的自定义字段
-            bo = Irm::BusinessObject.where(:bo_model_name => self.class.to_s).first
-            if self.external_system_id
-              custom_attributes = Irm::ObjectAttribute.list_all.real_field.query_by_business_object(bo.id).where("#{Irm::ObjectAttribute.table_name}.field_type = ? AND #{Irm::ObjectAttribute.table_name}.external_system_id=?","SYSTEM_CUX_FIELD", self.external_system_id)
-              custom_attributes +=  Irm::ObjectAttribute.list_all.real_field.query_by_business_object(bo.id).with_external_system(self.external_system_id).where("#{Irm::ObjectAttribute.table_name}.field_type = ?","GLOBAL_CUX_FIELD")
-            else
-              custom_attributes = []
-            end
-            custom_attributes
-          end
-        end
-      else
-        class_eval do
-          def custom_attributes
-            #取得对应model的自定义字段
-            bo = Irm::BusinessObject.where(:bo_model_name => self.class.to_s).first
-            Irm::ObjectAttribute.list_all.real_field.query_by_business_object(bo.id)
-          end
-        end
-      end
+      cattr_accessor :custom_field_options
+
+      self.custom_field_options = options
 
       class_eval do
         validate :validate_fields
 
         def validate_fields
           self.custom_attributes.each do |attribute|
+
             #检查是否必填
             if self[attribute[:attribute_name]].blank? and attribute[:required_flag].eql?('Y')
               self.errors.add attribute[:attribute_name], I18n.t("activerecord.errors.messages.blank")
             end
+
             #检查长度是否合适
             if self[attribute[:attribute_name]].present? and (attribute[:min_length] > 0 or attribute[:max_length] > 0)
               if  self[attribute[:attribute_name]].length < attribute[:min_length]
@@ -50,11 +32,27 @@ module Irm::CustomFields
                 self.errors.add attribute[:attribute_name], I18n.t("activerecord.errors.messages.too_long", :count => attribute[:max_length])
               end
             end
+
             #数字类型的添加判断
             if self[attribute[:attribute_name]].present? and "NUMBER".eql?(attribute[:category]) and self[attribute[:attribute_name]].match(/\D{1,}/)
               self.errors.add attribute[:attribute_name], I18n.t("activerecord.errors.messages.number")
             end
           end
+        end
+
+
+        #取得对应model的自定义字段
+        def custom_attributes
+          bo = Irm::BusinessObject.where(:bo_model_name => self.class.to_s).first
+          if self.custom_field_options[:system_flag] == 'Y' and self.external_system_id
+            custom_attributes = Irm::ObjectAttribute.multilingual.list_all.real_field.query_by_business_object(bo.id).where("#{Irm::ObjectAttribute.table_name}.field_type = ? AND #{Irm::ObjectAttribute.table_name}.external_system_id=?","SYSTEM_CUX_FIELD", self.external_system_id)
+            custom_attributes +=  Irm::ObjectAttribute.multilingual.list_all.real_field.query_by_business_object(bo.id).with_external_system(self.external_system_id).where("#{Irm::ObjectAttribute.table_name}.field_type = ?","GLOBAL_CUX_FIELD")
+          elsif self.custom_field_options[:system_flag] == 'N'
+            custom_attributes = Irm::ObjectAttribute.multilingual.list_all.real_field.query_by_business_object(bo.id)
+          else
+            custom_attributes = []
+          end
+          custom_attributes
         end
       end
     end
