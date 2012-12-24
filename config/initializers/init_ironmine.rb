a4a382ed570749477606cab10759c8cac21d12cd
uudui <442453867@qq.com>
2012-12-24 20:44:26 +0800

    暂时在初始化时候不加载smtp参数

--- config/initializers/init_ironmine.rb
+++ config/initializers/init_ironmine.rb
@@ -1,6 +1,6 @@
 begin
-  smtp_setting = Irm::SmtpSetting.first
-  smtp_setting.merge_settings if smtp_setting.present?
+  #smtp_setting = Irm::SmtpSetting.first
+  #smtp_setting.merge_settings if smtp_setting.present?
 #初始化系统参数
 #Irm::SystemParametersManager.prepare_system_parameters_cache
 end
