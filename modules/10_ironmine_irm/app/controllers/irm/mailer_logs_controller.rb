09a003cee320cb1e7bc897339cabfe18060623cd
uudui <442453867@qq.com>
2013-01-04 16:14:58 +0800

    邮件日志添加排根据发送时间进行排序

--- modules/10_ironmine_irm/app/controllers/irm/mailer_logs_controller.rb
+++ modules/10_ironmine_irm/app/controllers/irm/mailer_logs_controller.rb
@@ -4,7 +4,7 @@ class Irm::MailerLogsController < ApplicationController
   end
 
   def get_data
-    @mailer_logs = Irm::MailerLog
+    @mailer_logs = Irm::MailerLog.order_by_created_at
     @mailer_logs, count = paginate(@mailer_logs)
     respond_to do |format|
       format.html  {
