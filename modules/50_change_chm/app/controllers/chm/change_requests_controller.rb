c5b7d0bcd25ebf467063bc96792a53dc9b9bd5e0
uudui <442453867@qq.com>
2013-01-25 13:09:49 +0800

    格式化代码

--- modules/50_change_chm/app/controllers/chm/change_requests_controller.rb
+++ modules/50_change_chm/app/controllers/chm/change_requests_controller.rb
@@ -5,7 +5,7 @@ class Chm::ChangeRequestsController < ApplicationController
   def index
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  {
+      format.xml {
         @change_requests = Chm::ChangeRequest.all
         render :xml => @change_requests
       }
@@ -19,17 +19,17 @@ class Chm::ChangeRequestsController < ApplicationController
     @change_request = Chm::ChangeRequest.list_all.find(params[:id])
     @request_files = Chm::ChangeRequest.request_files(@change_request.id)
     @change_journals = @change_request.change_journals.list_all
-    @change_journal = @change_request.change_journals.build(:replied_by=>Irm::Person.current.id)
+    @change_journal = @change_request.change_journals.build(:replied_by => Irm::Person.current.id)
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @change_request }
-      format.pdf  {
+      format.xml { render :xml => @change_request }
+      format.pdf {
         @change_plan_types = Chm::ChangePlanType.multilingual.enabled
         @grouped_change_plans={}
-        change_plans =  @change_request.change_plans.list_all
+        change_plans = @change_request.change_plans.list_all
         @change_plan_types.each do |change_plan_type|
-          change_plan = change_plans.detect{|i| i.change_plan_type_id.eql?(change_plan_type.id)}
-          change_plan = Chm::ChangePlan.new(:change_plan_type_id=>change_plan_type.id,:change_request_id=>@change_request.id) unless change_plan.present?
+          change_plan = change_plans.detect { |i| i.change_plan_type_id.eql?(change_plan_type.id) }
+          change_plan = Chm::ChangePlan.new(:change_plan_type_id => change_plan_type.id, :change_request_id => @change_request.id) unless change_plan.present?
           @grouped_change_plans[change_plan_type.id] = change_plan
         end
         render :pdf => @change_request.title,
@@ -48,7 +48,7 @@ class Chm::ChangeRequestsController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.xml  { render :xml => @change_request }
+      format.xml { render :xml => @change_request }
     end
   end
 
@@ -59,7 +59,7 @@ class Chm::ChangeRequestsController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.xml  { render :xml => @change_request }
+      format.xml { render :xml => @change_request }
     end
   end
 
@@ -73,17 +73,17 @@ class Chm::ChangeRequestsController < ApplicationController
 
     @grouped_change_plans={}
 
-    change_plans =  @change_request.change_plans.list_all
+    change_plans = @change_request.change_plans.list_all
 
     @change_plan_types.each do |change_plan_type|
-      change_plan = change_plans.detect{|i| i.change_plan_type_id.eql?(change_plan_type.id)}
-      change_plan = Chm::ChangePlan.new(:change_plan_type_id=>change_plan_type.id,:change_request_id=>@change_request.id) unless change_plan.present?
+      change_plan = change_plans.detect { |i| i.change_plan_type_id.eql?(change_plan_type.id) }
+      change_plan = Chm::ChangePlan.new(:change_plan_type_id => change_plan_type.id, :change_request_id => @change_request.id) unless change_plan.present?
       @grouped_change_plans[change_plan_type.id] = change_plan
     end
 
     respond_to do |format|
       format.html
-      format.xml  { render :xml => @change_request }
+      format.xml { render :xml => @change_request }
     end
   end
 
@@ -94,10 +94,9 @@ class Chm::ChangeRequestsController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.xml  { render :xml => @change_request }
+      format.xml { render :xml => @change_request }
     end
   end
-
 
 
   # GET /statuses/1
@@ -107,7 +106,7 @@ class Chm::ChangeRequestsController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.xml  { render :xml => @change_request }
+      format.xml { render :xml => @change_request }
     end
   end
 
@@ -137,7 +136,7 @@ class Chm::ChangeRequestsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @change_request }
+      format.xml { render :xml => @change_request }
     end
   end
 
@@ -186,8 +185,8 @@ class Chm::ChangeRequestsController < ApplicationController
     end
 
     respond_to do |format|
-      format.html { render :action => "new"}
-      format.xml  { render :xml => @change_request }
+      format.html { render :action => "new" }
+      format.xml { render :xml => @change_request }
     end
   end
 
@@ -210,11 +209,11 @@ class Chm::ChangeRequestsController < ApplicationController
         process_files(@change_request)
         Icm::IncidentRequest.find(@change_request.incident_request_id).process_change(@change_request.id) if @change_request.incident_request_id.present?
 
-        format.html { redirect_to({:action => "show",:id=>@change_request.id}, :notice => t(:successfully_created)) }
-        format.xml  { render :xml => @change_request, :status => :created, :location => @change_request }
+        format.html { redirect_to({:action => "show", :id => @change_request.id}, :notice => t(:successfully_created)) }
+        format.xml { render :xml => @change_request, :status => :created, :location => @change_request }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @change_request.errors, :status => :unprocessable_entity }
+        format.xml { render :xml => @change_request.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -227,10 +226,10 @@ class Chm::ChangeRequestsController < ApplicationController
     respond_to do |format|
       if @change_request.update_attributes(params[:chm_change_request])
         format.html { redirect_to({:action => "index"}, :notice => t(:successfully_updated)) }
-        format.xml  { head :ok }
+        format.xml { head :ok }
       else
-        format.html { render :action => "edit",:layout => "application_full"}
-        format.xml  { render :xml => @change_request.errors, :status => :unprocessable_entity }
+        format.html { render :action => "edit", :layout => "application_full" }
+        format.xml { render :xml => @change_request.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -243,7 +242,7 @@ class Chm::ChangeRequestsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to(statuses_url) }
-      format.xml  { head :ok }
+      format.xml { head :ok }
     end
   end
 
@@ -263,39 +262,40 @@ class Chm::ChangeRequestsController < ApplicationController
                       :support_person_id_label,
                       :external_system_id,
                       :external_system_id_label,
-                      :category_id,:category_id_label,
+                      :category_id,
+                      :category_id_label,
                       :change_priority_id,
                       :change_priority_id_label,
                       :submitted_date]
-    bo = Irm::BusinessObject.where(:business_object_code=>"CHM_CHANGE_REQUESTS").first
+    bo = Irm::BusinessObject.where(:business_object_code => "CHM_CHANGE_REQUESTS").first
 
-    change_priority_table_alias = Irm::ObjectAttribute.get_ref_bo_table_name(bo.id,"change_priority_id")
+    change_priority_table_alias = Irm::ObjectAttribute.get_ref_bo_table_name(bo.id, "change_priority_id")
 
-    change_requests_scope = eval(bo.generate_query_by_attributes(return_columns,true))
+    change_requests_scope = eval(bo.generate_query_by_attributes(return_columns, true))
 
     if change_priority_table_alias.present?
       change_requests_scope.order("change_priority_table_alias.weight_values,#{bo.bo_table_name}.submitted_date DESC")
     end
 
-    change_requests_scope = change_requests_scope.match_value("#{bo.bo_table_name}.request_number",params[:request_number])
-    change_requests_scope = change_requests_scope.match_value("#{bo.bo_table_name}.title",params[:title])
+    change_requests_scope = change_requests_scope.match_value("#{bo.bo_table_name}.request_number", params[:request_number])
+    change_requests_scope = change_requests_scope.match_value("#{bo.bo_table_name}.title", params[:title])
 
     respond_to do |format|
       format.json {
-        change_requests_scope,count = paginate(change_requests_scope)
-        render :json=>to_jsonp(change_requests_scope.to_grid_json(return_columns,count))
+        change_requests_scope, count = paginate(change_requests_scope)
+        render :json => to_jsonp(change_requests_scope.to_grid_json(return_columns, count))
       }
       format.html {
-        @datas,@count = paginate(change_requests_scope)
+        @datas, @count = paginate(change_requests_scope)
       }
-      format.xls{
+      format.xls {
         change_requests = data_filter(change_requests_scope)
         send_data(data_to_xls(change_requests,
-                              [{:key=>:request_number,:label=>t(:label_chm_change_request_request_number)},
-                               {:key=>:title,:label=>t(:label_chm_change_request_title)},
-                               {:key=>:incident_status_id_label,:label=>t(:label_chm_change_request_change_status)},
-                               {:key=>:submitted_date,:label=>t(:label_chm_change_request_submitted_date)},
-                               {:key=>:external_system_id_label,:label=>t(:label_chm_change_request_external_system)}]
+                              [{:key => :request_number, :label => t(:label_chm_change_request_request_number)},
+                               {:key => :title, :label => t(:label_chm_change_request_title)},
+                               {:key => :incident_status_id_label, :label => t(:label_chm_change_request_change_status)},
+                               {:key => :submitted_date, :label => t(:label_chm_change_request_submitted_date)},
+                               {:key => :external_system_id_label, :label => t(:label_chm_change_request_external_system)}]
                   ))
       }
     end
@@ -307,19 +307,19 @@ class Chm::ChangeRequestsController < ApplicationController
     @change_request = Chm::ChangeRequest.list_all.find(params[:id])
 
     respond_to do |format|
-      format.html { render :layout=>"xhr"}
-      format.xml  { render :xml => @change_request }
+      format.html { render :layout => "xhr" }
+      format.xml { render :xml => @change_request }
     end
   end
 
   private
   def process_files(ref_request)
     @files = []
-    params[:files].each do |key,value|
-      @files << Irm::AttachmentVersion.create({:source_id=>ref_request.id,
-                                               :source_type=>ref_request.class.name,
-                                               :data=>value[:file],
-                                               :description=>value[:description]}) if(value[:file]&&!value[:file].blank?)
+    params[:files].each do |key, value|
+      @files << Irm::AttachmentVersion.create({:source_id => ref_request.id,
+                                               :source_type => ref_request.class.name,
+                                               :data => value[:file],
+                                               :description => value[:description]}) if (value[:file]&&!value[:file].blank?)
     end if params[:files]
   end
 end
