1e5c949c2b6a7d0f09d780a13eda32daf0714cc4
uudui <442453867@qq.com>
2013-03-12 17:23:19 +0800

    1、删除无效的代码； 2、添加批量删除班次功能，当删除右边的班次后将在日历中的所有班次都将被删除

--- modules/70_service_slm/app/controllers/slm/calendar_items_controller.rb
+++ modules/70_service_slm/app/controllers/slm/calendar_items_controller.rb
@@ -1,31 +1,4 @@
 class Slm::CalendarItemsController < ApplicationController
-  # GET /calendar_items/1
-  # GET /calendar_items/1.xml
-  def show
-    @calendar_item = Slm::CalendarItem.multilingual.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @calendar_item }
-    end
-  end
-
-  # GET /calendar_items/new
-  # GET /calendar_items/new.xml
-  def new
-    @calendar_item = Slm::CalendarItem.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @calendar_item }
-    end
-  end
-
-  # GET /calendar_items/1/edit
-  def edit
-    @calendar_item = Slm::CalendarItem.multilingual.find(params[:id])
-  end
-
   # POST /calendar_items
   # POST /calendar_items.xml
   def create
@@ -56,6 +29,17 @@ class Slm::CalendarItemsController < ApplicationController
     end
   end
 
+
+  def remove
+    @calendar_item = Slm::CalendarItem.find(params[:id])
+
+    @calendar_item.destroy
+
+    respond_to do |format|
+      format.json {render :json=> {:success => true}}
+    end
+  end
+
   # DELETE /calendar_items/1
   # DELETE /calendar_items/1.xml
   def destroy
@@ -70,37 +54,6 @@ class Slm::CalendarItemsController < ApplicationController
 
     respond_to do |format|
       format.json {render :json=> {:success => true}}
-    end
-  end
-
-  def multilingual_edit
-    @calendar_item = Slm::CalendarItem.find(params[:id])
-  end
-
-  def multilingual_update
-    @calendar_item = Slm::CalendarItem.find(params[:id])
-    @calendar_item.not_auto_mult=true
-    respond_to do |format|
-      if @calendar_item.update_attributes(params[:slm_calendar_item])
-        format.html { redirect_to({:action => "show"}, :notice => 'Calendar item was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @calendar_item.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  def get_data
-    calendar_items_scope = Slm::CalendarItem.multilingual.with_calendar(params[:calendar_id])
-    calendar_items_scope = calendar_items_scope.match_value("calendar_item.name",params[:name])
-    calendar_items,count = paginate(calendar_items_scope)
-    respond_to do |format|
-      format.html  {
-        @datas = calendar_items
-        @count = count
-      }
-      format.json {render :json=>to_jsonp(calendar_items.to_grid_json([:name,:description,:status_meaning],count))}
     end
   end
 end
