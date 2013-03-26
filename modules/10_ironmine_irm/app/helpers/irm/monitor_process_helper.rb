943c73f40677a91b7c3c8635aecce63162563188
uudui <442453867@qq.com>
2013-03-26 10:21:00 +0800

    完善进程状态显示，添加进程对应多语言

--- modules/10_ironmine_irm/app/helpers/irm/monitor_process_helper.rb
+++ modules/10_ironmine_irm/app/helpers/irm/monitor_process_helper.rb
@@ -12,13 +12,12 @@ module Irm::MonitorProcessHelper
     processes.each do |process|
       if process[:num] && process[:num] > 0
         tr_class = 'success'
-        status_name = t(:label_irm_monitor_proces_started)
+        status_name = "<span class='badge badge-success'><i class='icon-ok icon-white'></i>#{t(:label_irm_monitor_proces_started)}</span>"
         action_link = link_to(t(:label_irm_monitor_proces_stop), {:controller => "irm/monitor_process", :action => "stop_process", :id => process[:id], :_dom_id => "process_monitor" }, :class => "action-link", :remote => true)
       else
         tr_class = 'error'
-        status_name = t(:label_irm_monitor_proces_stoped)
+        status_name = "<span class='badge badge-important'><i class='icon-remove icon-white'></i>#{t(:label_irm_monitor_proces_stoped)}</span>"
         if process[:id].eql?("delayed_job")
-          #action_link = link_to(t(:label_irm_monitor_proces_start), {},"data-toggle" => "modal", :href => "#delayedJobModal", :class => "action-link", :id => "delayed_job_start_next")
           action_link = link_to(t(:label_irm_monitor_proces_start), {:controller => "irm/monitor_process", :action => "start_process", :id => process[:id], :_dom_id => "process_monitor" }, :class => "action-link", :id => "delayed_job_start")
         else
          action_link = link_to(t(:label_irm_monitor_proces_start) , {:controller => "irm/monitor_process", :action => "start_process", :id => process[:id], :_dom_id => "process_monitor" }, :class => "action-link", :remote => true)
