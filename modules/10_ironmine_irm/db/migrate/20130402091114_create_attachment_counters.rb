d33fe49131597f167092964e9a8ab3aeaeeb0808
uudui <442453867@qq.com>
2013-04-02 20:07:43 +0800

    文件管理添加历史版本查看和下载计数器功能

---
+++ modules/10_ironmine_irm/db/migrate/20130402091114_create_attachment_counters.rb
@@ -0,0 +1,20 @@
+class CreateAttachmentCounters < ActiveRecord::Migration
+  def up
+    create_table :irm_attachment_counters, :force => true do |t|
+      t.string "opu_id", :limit => 22, :collate => "utf8_bin"
+      t.string "status_code", :limit => 30, :null => false
+      t.string "version_id", :limit => 22, :collate => "utf8_bin"
+      t.string "download_by", :limit => 22, :collate => "utf8_bin"
+      t.string "created_by", :limit => 22, :collate => "utf8_bin"
+      t.string "updated_by", :limit => 22, :collate => "utf8_bin"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+    change_column :irm_attachment_counters, "id", :string, :limit => 22, :collate => "utf8_bin"
+  end
+
+  def down
+    drop_table :irm_attachment_counters
+  end
+
+end
