c5c4f438c0e9de23202eda1beb3086eb364f55e2
uudui <442453867@qq.com>
2013-04-07 14:33:05 +0800

    获取textarea中当前指针所在的位置，用户插入图片时进行定位

---
+++ modules/80_bootstrap_bsp/app/assets/javascripts/plugins/jquery-caretpos.js
@@ -0,0 +1,50 @@
+(function($) {
+    var getCaretPos, setCaretPos;
+    getCaretPos = function(inputor) {
+        var end, endRange, len, normalizedValue, pos, range, start, textInputRange;
+        if (document.selection) {
+            range = document.selection.createRange();
+            pos = 0;
+            if (range && range.parentElement() === inputor) {
+                normalizedValue = inputor.value.replace(/\r\n/g, "\n");
+                len = normalizedValue.length;
+
+                textInputRange = inputor.createTextRange();
+
+                textInputRange.moveToBookmark(range.getBookmark());
+                endRange = inputor.createTextRange();
+
+                endRange.collapse(false);
+                if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
+                    start = end = len;
+                } else {
+                    start = -textInputRange.moveStart("character", -len);
+                    end = -textInputRange.moveEnd("character", -len);
+                }
+            }
+        } else {
+            start = inputor.selectionStart;
+        }
+        return start;
+    };
+    setCaretPos = function(inputor, pos) {
+        var range;
+        if (document.selection) {
+            range = inputor.createTextRange();
+            range.move("character", pos);
+            return range.select();
+        } else {
+            return inputor.setSelectionRange(pos, pos);
+        }
+    };
+    return $.fn.caretPos = function(pos) {
+        var inputor;
+        inputor = this[0];
+        inputor.focus();
+        if (pos) {
+            return setCaretPos(inputor, pos);
+        } else {
+            return getCaretPos(inputor);
+        }
+    };
+})(window.jQuery);
