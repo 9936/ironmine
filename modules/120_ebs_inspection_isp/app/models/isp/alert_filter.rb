e978cb0b0b3c7e98f341bbfe6c07ae0877928715
uudui <442453867@qq.com>
2013-04-25 10:21:35 +0800

    完成巡检警告条件的基本功能及数据的校验

--- modules/120_ebs_inspection_isp/app/models/isp/alert_filter.rb
+++ modules/120_ebs_inspection_isp/app/models/isp/alert_filter.rb
@@ -2,6 +2,7 @@ class Isp::AlertFilter < ActiveRecord::Base
   set_table_name :isp_alert_filters
 
   validates_presence_of :check_item_id, :result_type, :operation, :value
+  validates_format_of :value, :with => /^\d+\.\d+$|^\d+$/i, :if => Proc.new { |i| i.result_type == 'NUMBER' }
 
   belongs_to :check_item, :foreign_key => :check_item_id
 
@@ -11,6 +12,6 @@ class Isp::AlertFilter < ActiveRecord::Base
 
   scope :with_operation_meaning, lambda {
     joins("JOIN #{Irm::LookupValue.view_name} lpv ON lpv.lookup_code = #{table_name}.operation").
-        where("lpv.language=?", I18n.locale).select("#{table_name}.*, lpv.meaning operation_name")
+        where("lpv.language=? AND lpv.lookup_type=?", I18n.locale, 'RULE_FILTER_OPERATOR').select("#{table_name}.*, lpv.meaning operation_name")
   }
 end
