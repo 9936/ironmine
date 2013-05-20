ae22ed753525d1d122cc101c07e1271b3488a43f
uudui <442453867@qq.com>
2013-05-20 21:24:10 +0800

    添加通过modal修改任务实例。

--- modules/80_bootstrap_bsp/app/assets/javascripts/bootstrap.js
+++ modules/80_bootstrap_bsp/app/assets/javascripts/bootstrap.js
@@ -5,35 +5,9 @@ $(function(){
 
   $("a[rel=modal]").live("click",function(e){
       e.preventDefault();
-      var href = $(this).attr('href'),
-          modalTitle = $(this).attr('modal_title') || "Loading....",
-          modalDialog = $("#linkModal");
+      var href = $(this).attr('href');
 
       openModal(href);
-
-//      if(modalDialog.length == 0){
-//          modalDialog = buildModal();
-//          $('body').append(modalDialog);
-//      }else{
-//          $(".modal-footer", modalDialog).html("");
-//      }
-//      $(".modal-header h3", modalDialog).text(modalTitle);
-//      $(".modal-body", modalDialog).html('<div class="loading-circle"></div>');
-//      modalDialog.modal('show');
-//      if(href.indexOf('#') == 0){
-//          $(href).modal('show');
-//      }else{
-//          href += href.indexOf("?") > 0 ? "&_dom_id=null" : "?_dom_id=null"
-//          modalDialog.load(href, function(){
-//              $('form', modalDialog).each(function(){
-//                  var $form = $(this),
-//                      $action = $form.attr("action");
-//                  $action += $action.indexOf("?") > 0 ? "&_dom_id=linkModal" : "?_dom_id=linkModal";
-//                  $form.data("remote", true);
-//                  $form.attr("action", $action);
-//              });
-//          });
-//      }
   });
 
   function openModal(href){
