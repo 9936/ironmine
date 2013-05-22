28e527077f6d343958ad699f3c97cefe3aa1be8e
hailor <hailor1985@gmail.com>
2013-05-22 17:51:29 +0800

    调整日常管理工作台界面之间的关系

--- modules/130_thing_gtd/lib/init_data.rb
+++ modules/130_thing_gtd/lib/init_data.rb
@@ -7,20 +7,36 @@ Fwk::MenuAndFunctionManager.map do |map|
   }
   map.function_group :task, {
       :zone_code => "TASK",
-      :controller => "gtd/tasks",
-      :action => "index"}
+      :controller => "gtd/task_workbenches",
+      :action => "today"}
   map.function_group :task, {
       :children => {
-          :task => {
-              :en => {:name => "Task", :description => "Task"},
-              :zh => {:name => "任务", :description => "任务"},
-              :default_flag => "Y",
+          :task_define => {
+              :en => {:name => "Task Management", :description => "Task Management"},
+              :zh => {:name => "日常保障工作管理", :description => "日常保障工作管理"},
+              :default_flag => "N",
               :login_flag => "N",
               :public_flag => "N",
                ##"edit_recurrence", "update_recurrence", "my_tasks_get_data", "my_tasks_index","get_assigned_data",
-              "gtd/tasks" => ["create","update", "edit", "get_data", "index", "new", "show", "get_calendar_data"],
-              "gtd/task_workbenches" => ["index","get_instance_data", "edit", "update","today","today_instance_data",
+              "gtd/tasks" => ["create","update", "edit", "get_data", "index", "new", "show", "get_calendar_data"]
+          },
+          :task_personal => {
+              :en => {:name => "Personal Task", :description => "Personal Task"},
+              :zh => {:name => "个人日常保障工作", :description => "个人日常保障工作"},
+              :default_flag => "Y",
+              :login_flag => "N",
+              :public_flag => "N",
+              "gtd/task_workbenches" => ["edit", "update","today","today_instance_data",
                                            "done", "update_done", "show"]
+          },
+          :task_admin => {
+              :en => {:name => "Task Monitor", :description => "Task Monitor"},
+              :zh => {:name => "日常保障工作监控", :description => "日常保障工作监控"},
+              :default_flag => "N",
+              :login_flag => "N",
+              :public_flag => "N",
+               ##"edit_recurrence", "update_recurrence", "my_tasks_get_data", "my_tasks_index","get_assigned_data",
+              "gtd/task_workbenches" => ["index","get_instance_data", "edit", "update"]
           }
       }
   }
