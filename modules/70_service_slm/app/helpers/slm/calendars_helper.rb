44872469732a74517b014e6e2031bb5bbd0775ef
uudui <442453867@qq.com>
2013-06-04 15:52:39 +0800

    修正96环境下无法打开日历

--- modules/70_service_slm/app/helpers/slm/calendars_helper.rb
+++ modules/70_service_slm/app/helpers/slm/calendars_helper.rb
@@ -1,7 +1,10 @@
 module Slm::CalendarsHelper
   #获取当前年所定义的所有班次
   def available_schedules(calendar)
+    puts "=============1111==============="
     calendar.calendar_items.where("calendar_year=?", Time.now.year).order("start_at ASC, end_at ASC")
+    puts "==============2222====================================="
+    []
   end
 
   def available_calendars(system_id)
