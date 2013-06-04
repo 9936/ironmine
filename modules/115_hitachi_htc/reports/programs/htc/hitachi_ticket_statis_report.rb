b8bb7ef44cd3f174b293b2a8aa8ad2e6338a0f99
stanleyliu <ivanmarkliu@gmail.com>
2013-06-04 09:16:58 +0800

    fixed: 修正HITACHI报表

--- modules/115_hitachi_htc/reports/programs/htc/hitachi_ticket_statis_report.rb
+++ modules/115_hitachi_htc/reports/programs/htc/hitachi_ticket_statis_report.rb
@@ -39,13 +39,13 @@ class Htc::HitachiTicketStatisReport < Irm::ReportManager::ReportBase
         where("incident_status_id IN (?) AND (cux_resolve_hours IS NULL OR (attribute1 LIKE ? AND (sattribute17 IS NULL OR sattribute19 IS NULL OR sattribute18 IS NULL OR sattribute20 IS NULL)))",
               close_status, "%System Bug%").
         where("NOT EXISTS (SELECT * FROM icm_incident_journals ij WHERE ij.incident_request_id = icm_incident_requests.id AND ij.reply_type = ? AND ij.created_at < ?)",
-              "CLOSE", Date.strptime("2012-10-31", '%Y-%m-%d')).
+              "CLOSE", Date.strptime("2012-11-01", '%Y-%m-%d')).
         select("count(1) hour_maintainance")
 
     root_cause = Icm::IncidentRequest.
         where("incident_status_id IN (?)", close_status).
         where("NOT EXISTS (SELECT * FROM icm_incident_journals ij WHERE ij.incident_request_id = icm_incident_requests.id AND ij.reply_type = ? AND ij.created_at < ?)",
-              "CLOSE", Date.strptime("2013-02-28", '%Y-%m-%d')).
+              "CLOSE", Date.strptime("2013-03-01", '%Y-%m-%d')).
         where("attribute1 IS NULL").
         select("count(1) root_cause")
 
@@ -54,7 +54,7 @@ class Htc::HitachiTicketStatisReport < Irm::ReportManager::ReportBase
                "Owner",
                "On-hand Open",
                "Deposit",
-               "Resolved",
+               "Resolved before > 3 days",
                "No Hour Maintainance",
                "No Root Cause"
                ]
