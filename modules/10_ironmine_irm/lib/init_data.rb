a8ec0c641423bd21412d39f519d31a7fb15e896a
uudui <442453867@qq.com>
2013-06-05 16:25:59 +0800

    修改API 对应的function group 名称。

--- modules/10_ironmine_irm/lib/init_data.rb
+++ modules/10_ironmine_irm/lib/init_data.rb
@@ -2881,8 +2881,8 @@ Fwk::MenuAndFunctionManager.map do |map|
   #=================================API TOOLS=================================
   #=================================START:Irm API=================================
   map.function_group :api_test_tool, {
-      :en => {:name => "API Test Tool", :description => "API Test Tool"},
-      :zh => {:name => "API测试工具", :description => "API测试工具"} }
+      :en => {:name => "API Tool", :description => "API Tool"},
+      :zh => {:name => "API工具", :description => "API工具"} }
   map.function_group :api_test_tool, {
       :zone_code => "API",
       :controller => "irm/api_tools",
@@ -2891,8 +2891,8 @@ Fwk::MenuAndFunctionManager.map do |map|
   map.function_group :api_test_tool, {
       :children => {
           :api_test_tool => {
-              :en => {:name => "API Test Tool", :description => "API Test Tool"},
-              :zh => {:name => "API测试工具", :description => "API测试工具"},
+              :en => {:name => "API Tool", :description => "API Tool"},
+              :zh => {:name => "API工具", :description => "API工具"},
               :default_flag => "N",
               :login_flag => "N",
               :public_flag => "N",
