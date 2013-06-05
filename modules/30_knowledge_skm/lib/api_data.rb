edeb404086893f4565e35056d70045cf37e07cfe
uudui <442453867@qq.com>
2013-06-05 09:37:33 +0800

    API参数添加Object类型支持

--- modules/30_knowledge_skm/lib/api_data.rb
+++ modules/30_knowledge_skm/lib/api_data.rb
@@ -7,7 +7,7 @@ Fwk::ApiParamsManager.map do |map|
           :params => [
               {:name => :id, :classify => [:output], :type => "String", :required => "Y", :description => "知识文章唯一ID"},
               {:name => :title, :classify => [:input,:output], :type => "String", :example_value => "如何通过API调用创建知识", :default_value => "" ,:required => "Y", :description => "知识库标题"},
-              {:name => :content,:classify => [:input,:output], :type => "String", :required => "Y", :description => "知识库内容"}
+              {:name => :content,:classify => [:input,:output], :type => "Object",:example_value => '{"contents:"[{"element_id_1:" "Please write content1 here" }, {"element_id_2:" "Please write content2 here" }, ... , {"element_N:", "Please write contentN here"}]', :required => "Y", :description => "知识库内容"}
           ],
       },
       :show => {
