cdfe083619ab848a1ef02be28bb44433e6972e14
uudui <442453867@qq.com>
2013-06-07 09:45:33 +0800

    修正API参数中的一些必填信息字段。

--- modules/30_knowledge_skm/lib/api_data.rb
+++ modules/30_knowledge_skm/lib/api_data.rb
@@ -9,8 +9,8 @@ Fwk::ApiParamsManager.map do |map|
               {:name => :full_search, :classify => [:input], :type => "String", :description => "全文检索，指明此参数，可检索出于关键字相关的知识文章"},
               {:name => :doc_number, :classify => [:input], :type => "String", :description => "知识文章编码"},
               {:name => :entry_title, :classify => [:input], :type => "String", :description => "知识文章标题"},
-              {:name => :start, :classify => [:input], :type => "Number", :default_value => 0, :description => "用于分页，从第几条记录开始"},
-              {:name => :limit, :classify => [:input], :type => "Number", :default_value => 10, :description => "每页显示记录数"},
+              {:name => :start, :classify => [:input], :type => "Number", :required => "Y", :default_value => 0, :description => "用于分页，从第几条记录开始"},
+              {:name => :limit, :classify => [:input], :type => "Number", :required => "Y", :default_value => 10, :description => "每页显示记录数"},
               {:name => :total_rows, :classify => [:output], :type => "Number", :default_value => 0, :description => "记录总数"},
               {:name => :items, :classify => [:output], :type => "Object", :description => "知识记录列表,其中每条记录中包含(id: 知识文章ID,is_favorite：是否被当前用户收藏,entry_status_code: 知识状态,full_title: 完整的文章标题，即[文章编码]+文章标题,entry_title: 文章标题,keyword_tags: 关键字,doc_number: 文章编码,version_number: 版本号,published_date: 发布日期,type_code: 类型编码)"}
           ]
@@ -21,8 +21,8 @@ Fwk::ApiParamsManager.map do |map|
           :name => "知识模板接口",
           :description => "知识模板接口",
           :params => [
-              {:name => :start, :classify => [:input], :type => "Number", :default_value => 0, :description => "用于分页，从第几条记录开始"},
-              {:name => :limit, :classify => [:input], :type => "Number", :default_value => 10, :description => "每页显示记录数"},
+              {:name => :start, :classify => [:input], :type => "Number", :required => "Y", :default_value => 0, :description => "用于分页，从第几条记录开始"},
+              {:name => :limit, :classify => [:input], :type => "Number", :required => "Y", :default_value => 10, :description => "每页显示记录数"},
               {:name => :total_rows, :classify => [:output], :type => "Number", :default_value => 0, :description => "记录总数"},
               {:name => :items, :classify => [:output], :type => "Object", :description => "模板列表，包括(id: 模板ID,entry_template_code: 模板编码,name: 模板名称,description: 模板说明"}
           ]
@@ -32,7 +32,7 @@ Fwk::ApiParamsManager.map do |map|
           :name => "创建知识接口",
           :description => "创建知识接口",
           :params => [
-              {:name => :id, :classify => [:output], :type => "String", :required => "Y", :description => "知识文章唯一ID"},
+              {:name => :id, :classify => [:output], :type => "String", :description => "知识文章唯一ID"},
               {:name => :entry_template_id, :classify => [:input, :output], :type => "String", :required => "Y", :description => "知识文章模板ID"},
               {:name => :entry_title, :classify => [:input, :output], :type => "String", :required => "Y", :description => "知识文章标题"},
               {:name => :keyword_tags, :classify => [:input, :output], :type => "String", :required => "Y", :description => "知识文章关键字"},
@@ -44,7 +44,7 @@ Fwk::ApiParamsManager.map do |map|
           :name => "查看知识接口",
           :description => "查看知识接口",
           :params => [
-              {:name => :id, :classify => [:input,:output], :type => "String", :description => "知识文章唯一ID"},
+              {:name => :id, :classify => [:input,:output], :type => "String", :required => "Y", :description => "知识文章唯一ID"},
               {:name => :entry_title, :classify => [:output], :type => "String", :description => "知识文章标题"},
               {:name => :keyword_tags, :classify => [:output], :type => "String", :description => "知识文章关键字"},
               {:name => :doc_number, :classify => [:output], :type => "String", :description => "知识文章文档号"},
