c22d6de5582d2752f9d60a50a0cb6a7970ea3e31
stanleyliu <ivanmarkliu@gmail.com>
2013-06-18 14:22:56 +0800

    fixed: migration bug

--- modules/116_yanmar_yan/db/migrate/20130618054751_add_system_to_journal.rb
+++ modules/116_yanmar_yan/db/migrate/20130618054751_add_system_to_journal.rb
@@ -3,7 +3,7 @@ class AddSystemToJournal < ActiveRecord::Migration
     add_column :icm_incident_journals, :external_system_id, :string, :limit => 22, :after => :opu_id
 
     Icm::IncidentJournal.all.each do |t|
-      if t.incident_reqeust
+      if t.incident_request
         t.update_attribute(:external_system_id, t.incident_request.external_system_id)
       end
     end
