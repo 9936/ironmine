96e2f32f8173564e4728b9443948ef2e0993f0a2
stanleyliu <ivanmarkliu@gmail.com>
2013-06-18 14:18:54 +0800

    fixed: YAN journal, add external system id

---
+++ modules/116_yanmar_yan/db/migrate/20130618054751_add_system_to_journal.rb
@@ -0,0 +1,12 @@
+class AddSystemToJournal < ActiveRecord::Migration
+  def change
+    add_column :icm_incident_journals, :external_system_id, :string, :limit => 22, :after => :opu_id
+
+    Icm::IncidentJournal.each do |t|
+      if t.incident_reqeust
+        t.update_attribute(:external_system_id, t.incident_request.external_system_id)
+      end
+    end
+
+  end
+end
--- modules/116_yanmar_yan/lib/yan/railtie.rb
+++ modules/116_yanmar_yan/lib/yan/railtie.rb
@@ -1,7 +1,7 @@
 module Yan
   class Railtie < Rails::Railtie
     config.to_prepare do
-
+      Icm::IncidentJournal.send(:include, Yan::IncidentJournalModelEx)
     end
   end
 end
