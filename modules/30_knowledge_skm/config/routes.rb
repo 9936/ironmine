28df0dadc33e189c493ad2852bebbcede53a9aee
uudui <442453867@qq.com>
2013-06-19 18:02:37 +0800

    API接口新增更新知识接口

--- modules/30_knowledge_skm/config/routes.rb
+++ modules/30_knowledge_skm/config/routes.rb
@@ -229,6 +229,7 @@ Rails.application.routes.draw do
       match '/api_entry_headers/get_channels' => "api_entry_headers#get_channels", :via => :get
       match '/api_entry_headers/get_columns' => "api_entry_headers#get_columns", :via => :get
       match '/api_entry_headers/add' => "api_entry_headers#add", :via => :post
+      match '/api_entry_headers/update' => "api_entry_headers#update", :via => :post
       match '/api_entry_headers/show' => "api_entry_headers#show", :via => :get
       match '/api_entry_headers/get_entry_books' => "api_entry_headers#get_entry_books", :via => :get
     end
