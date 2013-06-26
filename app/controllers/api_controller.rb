caae62ac257d30f6dadc12917c90dcf5a1531f88
uudui <442453867@qq.com>
2013-06-26 15:23:00 +0800

    修改API历史记录的参数标题

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,40 +3,39 @@ class ApiController < ApplicationController
   around_filter :set_access_history
 
   private
-    def set_access_history
-      access_history = Irm::OauthAccessHistory.new({:client_id => session[:client_id] || "api_tool",
-                                                   :access_by => Irm::Person.current.id,
-                                                   :accessed_at => Time.zone.now,
-                                                   #:access_api => url_for({:controller => params[:controller], :action => params[:action], :format => "json"}),
-                                                   :access_ip => request.remote_ip,
-                                                   :access_params => params.to_hash.to_s})
-      #根据controller和action查找出对应API的名称
-      rest_api = Irm::RestApi.query_by_controller_action(params[:controller], params[:action]).first
-      access_history.access_api = rest_api[:name] if rest_api.present?
-      begin
-        yield
-        access_history.success_flag = 'Y'
-      rescue Exception => e
-        access_history.success_flag = 'N'
-        access_history.error_message =e.message
-        respond_to do |format|
-          format.json {
-            render :json => {:error_info => e.message}
-          }
-        end
-
+  def set_access_history
+    access_history = Irm::OauthAccessHistory.new({:client_id => session[:client_id] || "api_tool",
+                                                  :access_by => Irm::Person.current.id,
+                                                  :accessed_at => Time.zone.now,
+                                                  :access_ip => request.remote_ip,
+                                                  :access_params => params.to_hash.to_s})
+    #根据controller和action查找出对应API的名称
+    rest_api = Irm::RestApi.query_by_controller_action(params[:controller], params[:action]).first
+    access_history.access_api = rest_api[:name] if rest_api.present?
+    begin
+      yield
+      access_history.success_flag = 'Y'
+    rescue Exception => e
+      access_history.success_flag = 'N'
+      access_history.error_message =e.message
+      respond_to do |format|
+        format.json {
+          render :json => {:error_info => e.message}
+        }
       end
-      access_history.save
-    end
 
-    def set_return_columns
-      @return_columns = []
-      output_params = Irm::ApiParam.get_output_params(params[:controller], params[:action])
-      output_params.each do |p|
-        @return_columns << p.name.to_sym
-      end
-      @return_columns
     end
+    access_history.save
+  end
+
+  def set_return_columns
+    @return_columns = []
+    output_params = Irm::ApiParam.get_output_params(params[:controller], params[:action])
+    output_params.each do |p|
+      @return_columns << p.name.to_sym
+    end
+    @return_columns
+  end
 
 
 end
