e235c23eacbd77bcbc93737cb698fab67fc592f7
stanleyliu <ivanmarkliu@gmail.com>
2013-07-01 22:00:49 +0800

    yan: att19

--- modules/116_yanmar_yan/lib/yan/incident_permission_check_helper_ex.rb
+++ modules/116_yanmar_yan/lib/yan/incident_permission_check_helper_ex.rb
@@ -16,5 +16,23 @@ module Yan::IncidentPermissionCheckHelperEx
         return false
       end
     end
+
+    #检查是否具有转交权限
+    def can_pass?(incident_request)
+      if incident_request.support_person_id.blank?
+        return false
+      end
+
+      if allow_to_function?(:pass_incident_request) || allow_to_function?(:system_pass_anyone, incident_request.external_system_id)
+        return true
+      elsif allow_to_function?(:system_pass_mine, incident_request.external_system_id) &&
+          (current_person?(incident_request.submitted_by) ||
+              current_person?(incident_request.support_person_id) ||
+              current_person?(incident_request.requested_by) )
+        return true
+      else
+        return false
+      end
+    end
   end
 end
