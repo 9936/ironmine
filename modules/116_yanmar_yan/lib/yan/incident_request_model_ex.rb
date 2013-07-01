9b27c43e233280803667ff24997fc1fae8a399bc
stanleyliu <ivanmarkliu@gmail.com>
2013-07-01 17:15:23 +0800

    yan: att21

---
+++ modules/116_yanmar_yan/lib/yan/incident_request_model_ex.rb
@@ -0,0 +1,8 @@
+module Yan::IncidentRequestModelEx
+  def self.included(base)
+    base.class_eval do
+      validates_presence_of :incident_category_id
+      validates_presence_of :incident_sub_category_id
+    end
+  end
+end
