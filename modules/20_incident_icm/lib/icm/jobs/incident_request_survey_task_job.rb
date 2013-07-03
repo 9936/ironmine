e2fd05d22b5831c3d610997605b636ca1d67c114
stanleyliu <ivanmarkliu@gmail.com>
2013-07-03 15:22:42 +0800

    fixed: 修复事故单创建后自动产生投票功能

--- modules/20_incident_icm/lib/icm/jobs/incident_request_survey_task_job.rb
+++ modules/20_incident_icm/lib/icm/jobs/incident_request_survey_task_job.rb
@@ -24,16 +24,32 @@ module Icm
           next if Csi::SurveyMember.where(:survey_id=>sv.id,:person_id=>person.id,:source_id=>request.id,:source_type=>request.class.name).first
           Delayed::Worker.logger.debug("IncidentRequestSurveyTaskJob Execute #{sv.id}")
 
-          person_ids =  Csi::SurveyRange.where(:survey_id=>self.id).query_person_ids.collect{|i| i[:person_id]}
-          Delayed::Worker.logger.debug("IncidentRequestSurveyTaskJob Find survey rang:#{sr.id} person ids:#{person_ids} current person #{person.id} ====#{person_ids.include?(person.id)}")
+          person_ids = []
+
+          srr = nil
+
+          Csi::SurveyRange.where(:survey_id=>sv.id).each do |sr|
+            person_ids = person_ids + Csi::SurveyRange.where(:survey_id=>sr.survey_id).query_person_ids.collect{|i| i[:person_id]}
+            if person_ids.include?(person.id)
+              srr = sr
+              break
+            end
+            Delayed::Worker.logger.debug("IncidentRequestSurveyTaskJob Find survey rang:#{sr.id} person ids:#{person_ids} current person #{person.id} ====#{person_ids.include?(person.id)}")
+          end
 
           next unless person_ids.include?(person.id)
-          sm = Csi::SurveyMember.where(:survey_id=>sv.id,:person_id=>person.id,:source_id=>request.id,:source_type=>request.class.name).first
-          if sm
+
+          sm = Csi::SurveyMember.where(:survey_id=>sv.id,:person_id=>person.id,:source_id=>request.id,:source_type=>request.class.name)
+
+          if sm.any?
             Delayed::Worker.logger.debug("IncidentRequestSurveyTaskJob update survey member #{sm.id}")
-            sm.update_attributes(:required_flag=>sr.required_flag,:end_date_active=>(sv.due_dates||24).hours.from_now) if sm
+            sm.each do |s|
+              s.update_attributes(:required_flag=>srr.required_flag,:end_date_active => sv.close_date) if sm
+            end
           else
-            sm=Csi::SurveyMember.create(:survey_id=>sv.id,:person_id=>person.id,:required_flag=>sr.required_flag,:response_flag=>Irm::Constant::SYS_NO,:end_date_active=>(sv.due_dates||24).hours.from_now,:source_id=>request.id,:source_type=>request.class.name)
+            sm = Csi::SurveyMember.create(:survey_id=>sv.id,:person_id=>person.id,:required_flag => srr.required_flag,
+                                        :response_flag=>Irm::Constant::SYS_NO,:end_date_active => sv.close_date,
+                                        :source_id=>request.id,:source_type=>request.class.name)
             Delayed::Worker.logger.debug("IncidentRequestSurveyTaskJob create survey member #{sm.id}")
           end
         end
