2b04405f0190de7a12f4dd89566a44b1bec02e09
uudui <442453867@qq.com>
2013-08-26 11:19:34 +0800

    1、修正地址中当不选择省份、城市或区域时导致地址信息无法显示； 2、当城市为四个直辖市时对地址的显示进行特殊处理。

--- modules/150_sugon_sug/db/migrate/20130816053803_create_addresses.rb
+++ modules/150_sugon_sug/db/migrate/20130816053803_create_addresses.rb
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 class CreateAddresses < ActiveRecord::Migration
   def change
     create_table :sug_addresses, :force => true do |t|
@@ -19,10 +21,13 @@ class CreateAddresses < ActiveRecord::Migration
     change_column :sug_addresses, "id", :string, :limit => 22, :collate => "utf8_bin"
     add_index :sug_addresses, [:source_id, :source_type], :name => "SUG_ADDRESSES_N1"
 
-    execute('CREATE OR REPLACE VIEW sug_addresses_vl AS SELECT sug_addresses.*, CONCAT(c.name,p.name,ci.name, d.name, sug_addresses.details) address_name FROM sug_addresses
-            JOIN sug_countries c ON c.id=sug_addresses.country_id
-            JOIN sug_provinces p ON p.id=sug_addresses.province_id
-            JOIN sug_cities ci ON ci.id=sug_addresses.city_id
-            JOIN sug_districts d ON d.id=sug_addresses.district_id')
+    execute("CREATE OR REPLACE VIEW sug_addresses_vl AS SELECT CONCAT(c.name,
+            CASE WHEN p.name IS NULL THEN '' ELSE p.name END,
+            CASE WHEN (ci.name IS NULL OR ci.name IN('北京市','上海市','重庆市','天津市')) THEN '' ELSE ci.name END,
+            CASE WHEN d.name IS NULL THEN '' ELSE d.name END,sug_addresses.details )address_name, sug_addresses.* FROM sug_addresses
+            LEFT JOIN sug_countries c ON c.id=sug_addresses.country_id
+            LEFT JOIN sug_provinces p ON p.id=sug_addresses.province_id
+            LEFT JOIN sug_cities ci ON ci.id=sug_addresses.city_id
+            LEFT JOIN sug_districts d ON d.id=sug_addresses.district_id")
   end
 end
