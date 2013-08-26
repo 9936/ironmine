40c6a943d654d3075109403cfc78aa7f0466ea6e
uudui <442453867@qq.com>
2013-08-26 19:54:16 +0800

    Person中添加技能合格的工程师查询scope方法。

--- modules/150_sugon_sug/lib/sug/person_model_ex.rb
+++ modules/150_sugon_sug/lib/sug/person_model_ex.rb
@@ -8,6 +8,31 @@ module Sug::PersonModelEx
       has_one :address, :class_name => "Sug::Address", :foreign_key => :source_id, :dependent => :destroy
       accepts_nested_attributes_for :address
 
+
+      #查找出含有指定类别的工程师，将含是否具有该机能的进行分开
+      scope :skilled_people, lambda{|country_id, province_id, city_id, district_id, category_id|
+        if country_id.present?
+          where_sql = "sa.country_id='#{country_id}' "
+          if province_id.present?
+            where_sql << "AND sa.province_id='#{province_id}' "
+            if city_id.present?
+              where_sql << "AND sa.city_id='#{city_id}' "
+              if district_id.present?
+                where_sql << "AND sa.district_id='#{district_id}' "
+              end
+            end
+          end
+        else
+          where_sql = "1=1"
+        end
+        where_sql << " AND cs.category_id='#{category_id}'"
+
+        joins("JOIN #{Sug::Address.table_name} sa ON sa.source_id=#{table_name}.id").
+            joins("JOIN #{Sug::PersonSkill.table_name} ps ON ps.person_id=#{table_name}.id").
+            joins("JOIN #{Sug::CategorySkill.table_name} cs ON cs.skill_id=ps.skill_id").
+            where(where_sql)
+      }
+
       def address_meaning
         address = Sug::Address.with_address_name.query_by_source(self.id, self.class.to_s).first
         if address.present?
