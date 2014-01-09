bbc7d9cc87969271216030bd7756759a5145984e
hailor <hailor1985@gmail.com>
2014-01-09 17:46:32 +0800

    修正销售状态和销售区域查询

--- modules/200_sales_opportunity_manager_som/app/models/som/sales_opportunity.rb
+++ modules/200_sales_opportunity_manager_som/app/models/som/sales_opportunity.rb
@@ -72,9 +72,19 @@ class Som::SalesOpportunity < ActiveRecord::Base
          select("sales.full_name sales_person_name")
    }
 
+   scope :with_status,lambda{|language|
+     joins("LEFT OUTER JOIN #{Irm::LookupValue.view_name} sale_status ON sale_status.lookup_type='SOM_PRODUCTION_STATUS' AND sale_status.lookup_code = #{table_name}.sales_status AND sale_status.language= '#{language}'").
+         select(" sale_status.meaning sales_status_meaning")
+   }
+
+
+   scope :with_region,lambda{|language|
+     joins("LEFT OUTER JOIN #{Irm::LookupValue.view_name} region ON region.lookup_type='SOM_REGION_INFO' AND region.lookup_code = #{table_name}.region AND region.language= '#{language}'").
+         select(" region.meaning region_meaning")
+   }
 
    def self.list_all
-     select_all.with_charger.with_customer.with_sales
+     select_all.with_charger.with_customer.with_sales.with_status(I18n.locale).with_region(I18n.locale)
    end
 
   def validate_before_save
