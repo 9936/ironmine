b2d23020a6bcf5c0d0b8923ddcffa944cd3bbf66
lissdy <lissdy033@gmail.com>
2014-01-16 13:59:08 +0800

    预销售:导出execl

--- modules/200_sales_opportunity_manager_som/app/controllers/som/sales_opportunities_controller.rb
+++ modules/200_sales_opportunity_manager_som/app/controllers/som/sales_opportunities_controller.rb
@@ -70,12 +70,12 @@ class Som::SalesOpportunitiesController < ApplicationController
   # DELETE /som/sales_opportunities/1.xml
   def destroy
     @sales_opportunity = Som::SalesOpportunity.find(params[:id])
-    if(current_person?(@sales_opportunity.created_by)||current_person?(@sales_opportunity.charge_person))
+    if (current_person?(@sales_opportunity.created_by)||current_person?(@sales_opportunity.charge_person))
       @sales_opportunity.destroy
     end
 
     respond_to do |format|
-      format.html { redirect_to(:action=>"index") }
+      format.html { redirect_to(:action => "index") }
       format.xml { head :ok }
     end
   end
@@ -88,41 +88,40 @@ class Som::SalesOpportunitiesController < ApplicationController
     session[:role]=params[:role]
     sales_opportunities_scope = Som::SalesOpportunity.list_all
     sales_opportunities_scope = sales_opportunities_scope.match_value("#{Som::SalesOpportunity.table_name}.name", params[:name])
+    #对可能性进行过渡
+    if params[:possibility].present?&&!params[:possibility].include?("all")
+      where_str = ""
+      where_params = []
+      params[:possibility].each { |p|
+        if where_str.present?
+          where_str << "OR (#{Som::SalesOpportunity.table_name}.possibility > ? and #{Som::SalesOpportunity.table_name}.possibility <= ?)"
+        else
+          where_str << "(#{Som::SalesOpportunity.table_name}.possibility > ? and #{Som::SalesOpportunity.table_name}.possibility <= ?)"
+        end
+        where_params << p.split("_").collect { |i| i.to_f*10 }
+      }
 
-      #对可能性进行过渡
-      if params[:possibility].present?&&!params[:possibility].include?("all")
-        where_str = ""
-        where_params = []
-        params[:possibility].each{|p|
-          if where_str.present?
-            where_str << "OR (#{Som::SalesOpportunity.table_name}.possibility > ? and #{Som::SalesOpportunity.table_name}.possibility <= ?)"
-          else
-            where_str << "(#{Som::SalesOpportunity.table_name}.possibility > ? and #{Som::SalesOpportunity.table_name}.possibility <= ?)"
-          end
-          where_params << p.split("_").collect{|i| i.to_f*10}
-        }
+      sales_opportunities_scope = sales_opportunities_scope.where(([where_str]+where_params).flatten)
 
-        sales_opportunities_scope = sales_opportunities_scope.where(([where_str]+where_params).flatten)
+    end
 
-      end
+    #对年份进行过渡
+    if params[:year].present?&&!params[:year].include?("all")
+      sales_opportunities_scope = sales_opportunities_scope.where("year(#{Som::SalesOpportunity.table_name}.start_at) in (?)", params[:year])
+    end
 
-      #对年份进行过渡
-      if params[:year].present?&&!params[:year].include?("all")
-        sales_opportunities_scope = sales_opportunities_scope.where("year(#{Som::SalesOpportunity.table_name}.start_at) in (?)",params[:year])
-      end
-
-      #对状态
-      if params[:status].present?&&!params[:status].include?("all")
-        status_filters=params[:status]
-        sales_opportunities_scope = sales_opportunities_scope.as_status(status_filters)
-      end
+    #对状态
+    if params[:status].present?&&!params[:status].include?("all")
+      status_filters=params[:status]
+      sales_opportunities_scope = sales_opportunities_scope.as_status(status_filters)
+    end
 
     #与事故单的关系
     if params[:role].present?&&!params[:role].include?("all")
-      if(params[:role].include?("CHARGE"))
-        sales_opportunities_scope = sales_opportunities_scope.where(:charge_person=>Irm::Person.current.id)
+      if (params[:role].include?("CHARGE"))
+        sales_opportunities_scope = sales_opportunities_scope.where(:charge_person => Irm::Person.current.id)
       else
-        sales_opportunities_scope = sales_opportunities_scope.where("#{Som::SalesOpportunity.table_name}.charge_person != ?",Irm::Person.current.id)
+        sales_opportunities_scope = sales_opportunities_scope.where("#{Som::SalesOpportunity.table_name}.charge_person != ?", Irm::Person.current.id)
       end
     end
 
@@ -141,7 +140,6 @@ class Som::SalesOpportunitiesController < ApplicationController
         when "second_price"
           sales_opportunities_scope = sales_opportunities_scope.order("#{Som::SalesOpportunity.table_name}.second_price  #{params[:order_value]}")
       end
-
     end
     sales_opportunities, count = paginate(sales_opportunities_scope)
     respond_to do |format|
@@ -150,7 +148,56 @@ class Som::SalesOpportunitiesController < ApplicationController
         @count = count
       }
       format.json { render :json => to_jsonp(sales_opportunities.to_grid_json([:name, :description, :status_meaning], count)) }
+      format.xls {
+        send_data(sales_data_to_xls(sales_opportunities_scope,
+                                    [{:key => :charge_person_name, :label => t(:label_som_sales_opportunity_charge_person)},
+                                     {:key => :name, :label => t(:label_som_sales_opportunity_alias_name)},
+                                     {:key => :content, :label => t(:label_som_sales_opportunity_alias_content)},
+                                     {:key => :potential_customer_name, :label => t(:label_som_sales_opportunity_customer)},
+                                     {:key => :region_meaning, :label => t(:label_som_sales_opportunity_region)},
+                                     {:key => :address, :label => t(:label_som_sales_opportunity_address)},
+                                     {:key => :price_year, :label => t(:label_som_sales_opportunity_price_year)},
+                                     {:key => :price, :label => t(:label_som_sales_opportunity_price)},
+                                     {:key => :total_price, :label => t(:label_som_sales_opportunity_total_price)},
+                                     {:key => :open_at_alias, :label => t(:label_som_sales_opportunity_sales_open_at)},
+                                     {:key => :previous_flag, :label => t(:label_som_sales_opportunity_sales_previous_flag)},
+                                     {:key => :sales_status_meaning, :label => t(:label_som_sales_opportunity_sales_alias_status)},
+                                     {:key => :possibility, :label => t(:label_som_sales_opportunity_sales_alias_possibility)},
+                                     {:key => :operation, :label => t(:label_som_sales_opportunity_operations)},
+                                     {:key => :hisms, :label => t(:label_som_sales_opportunity_hisms)},
+                                     {:key => :ebs, :label => t(:label_som_sales_opportunity_ebs)},
+                                     {:key => :siebel, :label => t(:label_som_sales_opportunity_siebel)},
+                                     {:key => "hr/peoplesoft", :label => t(:label_som_sales_opportunity_hr)},
+                                     {:key => :hyperion, :label => t(:label_som_sales_opportunity_hyperion)},
+                                     {:key => :ms, :label => t(:label_som_sales_opportunity_ms)},
+                                     {:key => :java, :label => t(:label_som_sales_opportunity_java)},
+                                     {:key => :sales_person_name, :label => t(:label_som_sales_opportunity_sales_alias_person)},
+                                     {:key => :internal_member, :label => t(:label_som_sales_opportunity_internal_member)},
+                                     {:key => :external_member, :label => t(:label_som_sales_opportunity_external_member)}
+                                    ]
+                  ))
+      }
     end
+  end
+
+
+  def sales_data_to_xls(datas, columns, options={})
+    datas.each do |data|
+      involved_production_info=data.involved_production_info
+      unless involved_production_info.nil?
+      involved_production_infos=involved_production_info.split(",")
+      involved_production_infos.each do |d|
+        data["#{d.downcase}"]=t("label_som_sales_opportunity_flag_Y")
+      end
+      end
+      if data.previous_flag.eql?("Y")
+        data[:previous_flag]=t("label_som_sales_opportunity_flag_Y")
+      else
+        data[:previous_flag]=t("label_som_sales_opportunity_flag_N")
+      end
+      data[:open_at_alias]=data[:open_at].strftime('%Y-%m-%d') unless data[:open_at].nil?
+    end
+    datas.to_xls(columns, options)
   end
 
 
