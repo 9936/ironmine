6688dacc43fc1af198d91ead84b9dc356be2f26f
stanleyliu <ivanmarkliu@gmail.com>
2014-01-24 14:49:43 +0800

    Fixed: HLI，修正migration report中的附件获取逻辑

--- modules/115_hitachi_htc/reports/programs/htc/cux_tickets_detail_migrate.rb
+++ modules/115_hitachi_htc/reports/programs/htc/cux_tickets_detail_migrate.rb
@@ -156,7 +156,7 @@ class Htc::CuxTicketsDetailMigrate < Irm::ReportManager::ReportBase
       g.uniq!
       data[18] = g.join(" | ")
       data[19] = s[:attribute1]
-      att_flag = Irm::AttachmentVersion.where("source_id = ?", s[:id]).size > 0 ? True : False
+      att_flag = Irm::AttachmentVersion.where("source_id = ?", s[:id]).size > 0 ? true : false
       if !att_flag && Irm::AttachmentVersion.where("source_id IN (?)", Icm::IncidentRequest.find(s[:id]).incident_journals.collect(&:id)).size > 0
         att_flag = att_flag || true
       else
