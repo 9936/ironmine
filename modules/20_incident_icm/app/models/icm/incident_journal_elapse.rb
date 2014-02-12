fce14a6bc02f16a73497a60a9aa7cf275fd987bc
hailor <hailor1985@gmail.com>
2014-02-12 11:24:07 +0800

    修改耗时统计功能,精确到人,并清除历史耗时数据,生成新的耗时数据

--- modules/20_incident_icm/app/models/icm/incident_journal_elapse.rb
+++ modules/20_incident_icm/app/models/icm/incident_journal_elapse.rb
@@ -12,9 +12,26 @@ class Icm::IncidentJournalElapse < ActiveRecord::Base
 
   belongs_to :incident_journal
 
-  private
+  scope :by_system,lambda{|external_system_id|
+     joins("JOIN #{Icm::IncidentJournal.table_name} ON #{Icm::IncidentJournal.table_name}.id = #{self.table_name}.incident_journal_id").
+         joins("JOIN #{Icm::IncidentRequest.table_name} ON #{Icm::IncidentRequest.table_name}.id = #{Icm::IncidentJournal..table_name}.incident_request_id").
+         where("#{Icm::IncidentRequest.table_name}.external_system_id = ?",external_system_id)
+  }
+
   def calculate_distance
      self.distance = self.end_at.to_i - self.start_at.to_i
-     self.real_distance = self.distance
+
+     work_calendar = Icm::IncidentWorkCalendar.where(:external_system_id=>self.incident_journal.incident_request.external_system_id).first
+     if work_calendar.present?
+       self.real_distance = work_calendar.work_time(self.start_at,self.end_at)
+     else
+       self.real_distance = self.distance
+     end
+
+  end
+
+
+  def self.recalculate_distance_by_system(external_system_id)
+    self.by_system(external_system_id).each{|i| i.calculate_distance}
   end
 end
