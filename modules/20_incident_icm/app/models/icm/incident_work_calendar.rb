7c744b2c219c768c792f8fa1701a893a2d641629
lissdy <lissdy033@gmail.com>
2014-02-12 11:35:10 +0800

    为Icm::IncidentWorkCalendar添加方法计算两个时间的时间间隔

--- modules/20_incident_icm/app/models/icm/incident_work_calendar.rb
+++ modules/20_incident_icm/app/models/icm/incident_work_calendar.rb
@@ -4,4 +4,12 @@ class Icm::IncidentWorkCalendar < ActiveRecord::Base
   def self.incident_work_calendar(external_system_id)
     self.where(:external_system_id => external_system_id).first||self.new(:external_system_id => external_system_id)
   end
+
+  #计算两个时间的间隔
+  def working_time(start_time,end_time)
+    calendar = Slm::Calendar.find(self.id)
+    time_zone = self.time_zone
+    calendar.working_time_with_zone(time_zone,start_time,end_time)
+  end
+
 end
