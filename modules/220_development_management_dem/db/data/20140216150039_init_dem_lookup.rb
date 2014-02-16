fbb13d72d9c54be2b4a26cae247cc5728c9e7dd0
stanleyliu <ivanmarkliu@gmail.com>
2014-02-17 00:47:39 +0800

    Fixed: Fix lookup migration bug

--- modules/220_development_management_dem/db/data/20140216150039_init_dem_lookup.rb
+++ modules/220_development_management_dem/db/data/20140216150039_init_dem_lookup.rb
@@ -2,8 +2,8 @@
 class InitDemLookup < ActiveRecord::Migration
   def up
     lt =Irm::LookupType.new(:lookup_level=>'GLOBAL',:lookup_type=>'DEM_PROJECT',:status_code=>'ENABLED',:not_auto_mult=>true)
-    lt.lookup_types_tls.build(:lookup_type_id=> sug_sex.id,:meaning=>'Project',:description=>'Project',:language=>'zh',:status_code=>'ENABLED',:source_lang=>'en')
-    lt.lookup_types_tls.build(:lookup_type_id=> sug_sex.id,:meaning=>'Project',:description=>'Project',:language=>'en',:status_code=>'ENABLED',:source_lang=>'en')
+    lt.lookup_types_tls.build(:lookup_type_id=> lt.id,:meaning=>'Project',:description=>'Project',:language=>'zh',:status_code=>'ENABLED',:source_lang=>'en')
+    lt.lookup_types_tls.build(:lookup_type_id=> lt.id,:meaning=>'Project',:description=>'Project',:language=>'en',:status_code=>'ENABLED',:source_lang=>'en')
     lt.save
 
     lv = Irm::LookupValue.new(:lookup_type=>'DEM_PROJECT',:lookup_code=>'YEU_GAP',:start_date_active=>'2012-02-11',:status_code=>'ENABLED',:not_auto_mult=>true)
