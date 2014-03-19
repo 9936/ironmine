042560b62849bd864e35e9a9e5e5d6819cf4cc5c
stanleyliu <ivanmarkliu@gmail.com>
2014-03-19 22:58:05 +0800

    Fix: HLI, 复制 马波斯 的SLA配置到其它HOTLINE项目中

--- modules/110_hotline_hli/db/data/20140319121854_copy_sla_data.rb
+++ modules/110_hotline_hli/db/data/20140319121854_copy_sla_data.rb
@@ -15,7 +15,7 @@ class CopySlaData < ActiveRecord::Migration
     source_calendars = Slm::Calendar.where("external_system_id = ?", source_project.id)
 
     #source agreements
-    source_agreements = Slm::ServiceAgreement.where("external_system_id = ?", source_project.id).enabled
+    source_agreements = Slm::ServiceAgreement.where("external_system_id = ?", source_project.id)
     puts("+++++++++start copy")
     #starting copy
     hotline_projects.each do |hp|
@@ -29,47 +29,57 @@ class CopySlaData < ActiveRecord::Migration
           sct_attributes = sct.attributes
           sct_attributes.delete("id")
           sct_attributes.delete("calendar_id")
+          sct_attributes.delete("created_at")
+          sct_attributes.delete("updated_at")
           t.calendars_tls.build(sct_attributes)
         end
         t.save
         sc.calendar_items.each do |ci|
           ci_attributes = ci.attributes
           ci_attributes.delete("id")
+          ci_attributes.delete("created_at")
+          ci_attributes.delete("updated_at")
           new_ci = Slm::CalendarItem.new(ci_attributes)
           new_ci.calendar_id = t.id
           new_ci.save
         end
-      end
-      puts("+++++++++finish Calendar")
-      #copy agreements、trigger、trigger actions
-      source_agreements.each do |sa|
-        sa_attributes = sa.attributes
-        sa_attributes.delete("id")
-        t = Slm::ServiceAgreement.new(sa_attributes.merge({:not_auto_mult=>true}))
-        t.external_system_id = hp.id
-        sa.service_agreements_tls.each do |sat|
-          sat_attributes = sat.attributes
-          sat_attributes.delete("id")
-          sat_attributes.delete("service_agreement_id")
-          t.service_agreements_tls.build(sat_attributes)
-        end
-        t.save
-        sa.time_triggers.each do |tt|
-          tt_attributes = tt.attributes
-          tt_attributes.delete("id")
-          new_tt = Slm::TimeTrigger.new(tt_attributes)
-          new_tt.service_agreement_id = t.id
-          new_tt.save
-          tt.time_trigger_actions.each do |tta|
-            tta_attributes = tta.attributes
-            tta_attributes.delete("id")
-            new_tta = Slm::TimeTriggerAction.new(tta_attributes)
-            new_tta.time_trigger_id = new_tt.id
-            new_tta.save
+
+        #copy agreements、trigger、trigger actions
+        source_agreements.where("calendar_id = ?", sc.id).each do |sa|
+          sa_attributes = sa.attributes
+          sa_attributes.delete("id")
+          sa_attributes.delete("created_at")
+          sa_attributes.delete("updated_at")
+          ts = Slm::ServiceAgreement.new(sa_attributes.merge({:not_auto_mult=>true}))
+          ts.external_system_id = hp.id
+          ts.calendar_id = t.id
+          sa.service_agreements_tls.each do |sat|
+            sat_attributes = sat.attributes
+            sat_attributes.delete("id")
+            sat_attributes.delete("service_agreement_id")
+            ts.service_agreements_tls.build(sat_attributes)
+          end
+          ts.save
+          sa.time_triggers.each do |tt|
+            tt_attributes = tt.attributes
+            tt_attributes.delete("id")
+            tt_attributes.delete("created_at")
+            tt_attributes.delete("updated_at")
+            new_tt = Slm::TimeTrigger.new(tt_attributes)
+            new_tt.service_agreement_id = t.id
+            new_tt.save
+            tt.time_trigger_actions.each do |tta|
+              tta_attributes = tta.attributes
+              tta_attributes.delete("id")
+              tta_attributes.delete("created_at")
+              tta_attributes.delete("updated_at")
+              new_tta = Slm::TimeTriggerAction.new(tta_attributes)
+              new_tta.time_trigger_id = new_tt.id
+              new_tta.save
+            end
           end
         end
       end
-      puts("+++++++++finish ServiceAgreement")
     end
 
   end
