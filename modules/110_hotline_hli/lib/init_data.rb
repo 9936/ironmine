3f638f9b297de53e9c7a735f22ffd61474967083
stanley <stanley.liu@hand-china.com>
2014-03-31 22:34:14 +0800

    Feature: MASTER, HLI, CUX field permission

--- modules/110_hotline_hli/lib/init_data.rb
+++ modules/110_hotline_hli/lib/init_data.rb
@@ -100,7 +100,44 @@ map.function_group :hotline_project, {
               :public_flag => "N",
               :system_flag => "Y",
               "icm/incident_journals" => ["update_status"]
-          }
+          },
+          :edit_additional_info => {
+              :zh => {:name => "附加信息维护", :description => "附加信息维护"},
+              :en => {:name => "Edit Additional Info.", :description => "Edit Additional Info."},
+              :default_flag => "N",
+              :login_flag => "N",
+              :public_flag => "N",
+              :system_flag => "Y",
+              "icm/incident_journals" => ["new"]
+          },
+          :additional_info_area1 => {
+              :zh => {:name => "附加信息1区", :description => "附加信息1区"},
+              :en => {:name => "Additional Info. 1", :description => "Additional Info. 1"},
+              :default_flag => "N",
+              :login_flag => "N",
+              :public_flag => "N",
+              :system_flag => "Y",
+              "icm/incident_journals" => ["new"]
+          },
+          ##状态
+          :additional_info_area2 => {
+              :zh => {:name => "附加信息2区", :description => "附加信息2区"},
+              :en => {:name => "Additional Info. 2", :description => "Additional Info. 2"},
+              :default_flag => "N",
+              :login_flag => "N",
+              :public_flag => "N",
+              :system_flag => "Y",
+              "icm/incident_journals" => ["new"]
+          },
+          :additional_info_area3 => {
+              :zh => {:name => "附加信息3区", :description => "附加信息3区"},
+              :en => {:name => "Additional Info. 3", :description => "Additional Info. 3"},
+              :default_flag => "N",
+              :login_flag => "N",
+              :public_flag => "N",
+              :system_flag => "Y",
+              "icm/incident_journals" => ["new"]
+          },
       }
   }
 end
