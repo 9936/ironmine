e9b7cc342de653982b1b05ba07a00a8e5d6c98b5
stanley <stanley.liu@hand-china.com>
2014-05-21 16:39:01 +0800

    Feature: MASTER, NDM, Fix DEM bugs

--- modules/221_new_dev_management_ndm/app/controllers/ndm/dev_phases_controller.rb
+++ modules/221_new_dev_management_ndm/app/controllers/ndm/dev_phases_controller.rb
@@ -24,15 +24,6 @@ class Ndm::DevPhasesController < ApplicationController
   end
 
   def destroy
-    @dev_phase = Dem::DevPhase.find(params[:id])
-    tid = @dev_phase.dev_management_id
-    @dev_phase.destroy
-    Dem::DevManagement.find(tid).update_trigger
-    respond_to do |format|
-      format.html { redirect_to({:controller => "ndm/dev_managements",
-                                 :action => "edit", :id => tid},
-                                :notice => t(:successfully_updated)) }
-      format.xml { head :ok }
-    end
+
   end
 end
