54e53ca15408a89cfbbc7c78c671846299436658
stanley <stanley.liu@hand-china.com>
2014-06-17 13:31:57 +0800

    Feature: MASTER, HLI, FIX REPLY BUG

--- modules/110_hotline_hli/db/migrate/20140512043144_add_journal_id_to_workload.rb
+++ modules/110_hotline_hli/db/migrate/20140512043144_add_journal_id_to_workload.rb
@@ -1,4 +1,4 @@
-class AddJournalIdToWorkloadt < ActiveRecord::Migration
+class AddJournalIdToWorkload < ActiveRecord::Migration
   def change
     add_column :icm_incident_journals, :workload,  :string, :limit => 5, :after => :opu_id, :default => '0'
   end
