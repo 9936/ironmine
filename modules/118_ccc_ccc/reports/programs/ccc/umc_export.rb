92ed675fb75995a7b462e302f9b2e28a456db4d4
stanley <stanley.liu@hand-china.com>
2014-07-24 16:36:19 +0800

    Feature: MASTER, CCC, umc export

---
+++ modules/118_ccc_ccc/reports/programs/ccc/umc_export.rb
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+class Ccc::UmcExport < Irm::ReportManager::ReportBase
+  def data(params={})
+    project_id = ""
+    params||={}
+
+    statis = Icm::IncidentRequest.
+        select_all.
+        enabled.
+        with_category(I18n.locale).
+        with_close_reason(I18n.locale).
+        with_requested_by(I18n.locale).
+        with_incident_status(I18n.locale).
+        with_supporter(I18n.locale).
+        with_submitted_by.
+        with_priority(I18n.locale).
+        with_external_system(I18n.locale).
+        order("(#{Icm::IncidentRequest.table_name}.submitted_date) ASC")
+    end_date = params[:end_date]
+    unless params[:end_date].present?
+      end_date = "2099-1-1"
+    end
+    if params[:end_date].present?
+      statis = statis.where("date_format(icm_incident_requests.submitted_date, '%Y-%m-%d') <= ?", Date.strptime("#{end_date}", '%Y-%m-%d').strftime("%Y-%m-%d"))
+    end
+
+    start_date = params[:start_date]
+    unless params[:start_date].present?
+       start_date = "1970-1-1"
+    end
+    statis = statis.where("date_format(icm_incident_requests.submitted_date, '%Y-%m-%d') >= ?", Date.strptime("#{start_date}", '%Y-%m-%d').strftime("%Y-%m-%d"))
+
+    if params[:external_system_id].present? && params[:external_system_id].size > 0 && params[:external_system_id][0].present?
+      statis = statis.where("external_system.id IN (?)", params[:external_system_id] + [])
+    else
+      current_acc_systems = Irm::ExternalSystem.multilingual.order_with_name.with_person(params[:running_person_id]).enabled.collect(&:id)
+      statis = statis.where("external_system.id IN (?)", current_acc_systems + []) unless Irm::Person.where("login_name = ?",'anonymous').where("id = ?", params[:running_person_id]).any?
+    end
+
+    datas = []
+
+
+    headers = [
+                "问题编号",
+                "问题提交时间",
+                "问题结束时间",
+                "优先级",
+                "问题状态",
+                "问题描述",
+                "分派给",
+                "项目名称",
+                "问题提交人",
+                "参与者",
+                "最后更新时间",
+                "问题分类"
+               ]
+
+   
+    statis.each do |s|
+      data = Array.new(12)
+      data[0] = s[:request_number]
+      data[1] = s[:submitted_date]
+      last_close_journal = Icm::IncidentJournal.
+          where("incident_request_id = ?", s.id).
+          where("reply_type = ?", "CLOSE").
+          select("created_at").
+          order("created_at DESC").limit(1)
+      if last_close_journal.any?
+        data[2] = last_close_journal.first[:created_at]
+      else
+        data[2] = ""
+      end
+      data[3] = s[:priority_name]
+      data[4] = s[:incident_status_name]
+      data[5] = s[:title]
+      data[6] = s[:supporter_name]
+      data[7] = s[:external_system_name]
+      data[8] = s[:submitted_name]
+      watchers = s.watchers
+      if !watchers.nil? && watchers.any?
+        data[9] = s.watchers.collect(&:full_name).join(',')
+      else
+        data[9] = ""
+      end
+      data[10] = s[:last_response_date]
+      data[11] = s[:incident_category_name]
+
+      datas << data
+    end
+
+    {:datas=>datas,:headers=>headers,:params=>params}
+  end
+
+  def to_xls(params)
+    columns = []
+
+    result = data(params)
+
+    result[:headers].each_with_index do |sh,index|
+      columns << {:key=>index.to_s.to_sym,:label=>sh}
+    end
+
+    excel_data = []
+    result[:datas].each_with_index do |data,index|
+      excel_data << data.to_cus_hash
+    end
+
+    excel_data.to_xls(columns,{})
+  end
+end
