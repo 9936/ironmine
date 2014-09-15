6f913a3400a7610a03030f2e1ccb9dfb84806ee3
stanley <stanley.liu@hand-china.com>
2014-09-15 10:57:09 +0800

    Feature: MASTER,yanmar, boa new request

--- modules/222_board_boa/app/controllers/boa/boards_controller.rb
+++ modules/222_board_boa/app/controllers/boa/boards_controller.rb
@@ -7,21 +7,56 @@ class Boa::BoardsController < ApplicationController
     @table_a_accum_close_amount = []
     @table_a_incident_by_category_open = []
     @table_a_incident_by_category_total = []
+    @table_a_open_by_service_desk = []
+
+    @table_a_open_by_service_desk = Icm::IncidentRequest.enabled.
+        joins(",#{Icm::IncidentStatus.view_name} isv").
+        joins(",icm_support_groups_vl isg").
+        where("isg.language = 'zh'").
+        where("isg.id = #{Icm::IncidentRequest.table_name}.support_group_id").
+        where("isg.name = 'Service Desk'").
+        select("isv.name status_name, SUM(1) amount, isv.display_color").
+        where("isv.id = #{Icm::IncidentRequest.table_name}.incident_status_id").
+        where("isv.language = 'en'").
+        where("NOT EXISTS (SELECT 1 FROM icm_incident_journals ij WHERE ij.reply_type = 'CLOSE' AND ij.incident_request_id = #{Icm::IncidentRequest.table_name}.id)").
+        group("#{Icm::IncidentRequest.table_name}.incident_status_id").order("isv.display_sequence + 0 ASC").collect{|i| [i[:status_name], i[:amount].to_i, i[:display_color]]}
 
     @table_a_incident_by_category_open = Icm::IncidentRequest.enabled.joins(",#{Icm::IncidentCategory.view_name} ic").
         select("ic.name category_name, SUM(1) amount").
+        joins(",icm_support_groups_vl isg").
+        where("isg.language = 'zh'").
+        where("isg.id = #{Icm::IncidentRequest.table_name}.support_group_id").
+        where("isg.name = 'Service Desk'").
         where("ic.id = #{Icm::IncidentRequest.table_name}.incident_category_id").
         where("ic.language = 'en'").
         where("NOT EXISTS (SELECT 1 FROM icm_incident_journals ij WHERE ij.reply_type = 'CLOSE' AND ij.incident_request_id = #{Icm::IncidentRequest.table_name}.id)").
-        group("#{Icm::IncidentRequest.table_name}.incident_category_id").collect{|i| [i[:category_name], i[:amount].to_i]}
+        group("#{Icm::IncidentRequest.table_name}.incident_category_id").order("ic.code + 0 ASC").collect{|i| [i[:category_name], i[:amount].to_i, '']}
+
+    @table_a_incident_by_category_open.each do |c|
+      c[2] = '#FF7385' if c[0].eql?("Failure")
+      c[2] = '#E8AB5D' if c[0].eql?("Inquiry")
+      c[2] = '#FFFEC7' if c[0].eql?("Change Request")
+      c[2] = '#66D6FF' if c[0].eql?("Regular Maintenance")
+      c[2] = '#84FF82' if c[0].eql?("Non-Regular Maintenance")
+      c[2] = '#8E5DE8' if c[0].eql?("Master Maintenance")
+      c[2] = '#A9E2E8' if c[0].eql?("EBS")
+    end
 
     @table_a_incident_by_category_total = Icm::IncidentRequest.enabled.joins(",#{Icm::IncidentCategory.view_name} ic").
+        joins(",icm_support_groups_vl isg").
+        where("isg.language = 'zh'").
+        where("isg.id = #{Icm::IncidentRequest.table_name}.support_group_id").
+        where("isg.name = 'Service Desk'").
         select("ic.name category_name, SUM(1) amount").
         where("ic.id = #{Icm::IncidentRequest.table_name}.incident_category_id").
         where("ic.language = 'en'").
         group("#{Icm::IncidentRequest.table_name}.incident_category_id").collect{|i| [i[:category_name], i[:amount].to_i]}
 
     ir_submited_result = Icm::IncidentRequest.enabled.
+        joins(",icm_support_groups_vl isg").
+        where("isg.language = 'zh'").
+        where("isg.id = #{Icm::IncidentRequest.table_name}.support_group_id").
+        where("isg.name = 'Service Desk'").
         select("DATE_FORMAT(submitted_date, '%Y-%m-%d') submitted_date_formatted, SUM(1) submitted_amount").
         where("submitted_date >= ?", (Time.now - 30.days).strftime('%Y-%m-%d')).
         group("DATE_FORMAT(submitted_date, '%Y-%m-%d')").collect{|a| [a[:submitted_date_formatted], a[:submitted_amount]]}
@@ -145,7 +180,7 @@ class Boa::BoardsController < ApplicationController
       @issue_realtime_result_array << cell_array
     end
 
-    for today in (Time.now - 30.days).strftime('%Y-%m-%d').to_datetime..Time.now.strftime('%Y-%m-%d').to_datetime do
+    for today in (Time.now - 7.days).strftime('%Y-%m-%d').to_datetime..Time.now.strftime('%Y-%m-%d').to_datetime do
       ir_submitted = ir_submited_result.select{|i| i[0] == today.strftime('%Y-%m-%d')}
       ir_closed = ir_close_result.select{|i| i[0] == today.strftime('%Y-%m-%d')}
       @table_a_date << today.strftime('%m/%d')
