e4125cd6fa6e7e2e2b688670e334d6afb5f01469
stanley <stanley.liu@hand-china.com>
2014-11-11 10:52:03 +0800

    Feature: YAN, FIX CUX MONTH STAT REPORT GROUP BLANKABLE

--- modules/116_yanmar_yan/reports/programs/yan/cux_month_stat.rb
+++ modules/116_yanmar_yan/reports/programs/yan/cux_month_stat.rb
@@ -27,28 +27,47 @@ class Yan::CuxMonthStat < Irm::ReportManager::ReportBase
       system = Irm::ExternalSystem.multilingual.where("#{Irm::ExternalSystem.table_name}.id = ?", params[:external_system_id])
       data[0] = system.first[:system_name]
       data[1] = today.strftime('%Y-%m-%d').to_s
-      n = Icm::IncidentRequest.
-          where("date_format(#{Icm::IncidentRequest.table_name}.submitted_date, '%Y-%m-%d') = ?",
-                today.strftime('%Y-%m-%d')).
-          where("#{Icm::IncidentRequest.table_name}.external_system_id = ?", params[:external_system_id])
-      n = n.where("#{Icm::IncidentRequest.table_name}.support_group_id = ?", params[:support_group_id]) unless params[:support_group_id].blank?
-      data[2] = n.size.to_s
+      unless params[:support_group_id].blank?
+        n = Icm::IncidentRequest.
+            where("date_format(#{Icm::IncidentRequest.table_name}.submitted_date, '%Y-%m-%d') = ?",
+                  today.strftime('%Y-%m-%d')).where("#{Icm::IncidentRequest.table_name}.support_group_id = ?", params[:support_group_id]).
+            where("#{Icm::IncidentRequest.table_name}.external_system_id = ?", params[:external_system_id])
+        data[2] = n.size.to_s
 
-      c = Icm::IncidentRequest.
-          where("EXISTS(SELECT 1 FROM icm_incident_journals ij WHERE ij.incident_request_id = #{Icm::IncidentRequest.table_name}.id AND ij.reply_type = 'CLOSE' AND  DATE_FORMAT(ij.created_at, '%Y-%m-%d') = ?)",
-                today.strftime('%Y-%m-%d')).
-          where("#{Icm::IncidentRequest.table_name}.external_system_id = ?", params[:external_system_id])
-      c = c.where("#{Icm::IncidentRequest.table_name}.support_group_id = ?", params[:support_group_id]) unless params[:support_group_id].blank?
-      data[3] = c.size.to_s
+        c = Icm::IncidentRequest.
+            where("EXISTS(SELECT 1 FROM icm_incident_journals ij WHERE ij.incident_request_id = #{Icm::IncidentRequest.table_name}.id AND ij.reply_type = 'CLOSE' AND  DATE_FORMAT(ij.created_at, '%Y-%m-%d') = ?)",
+                  today.strftime('%Y-%m-%d')).where("#{Icm::IncidentRequest.table_name}.support_group_id = ?", params[:support_group_id]).
+            where("#{Icm::IncidentRequest.table_name}.external_system_id = ?", params[:external_system_id])
+        data[3] = c.size.to_s
 
-      e = Icm::IncidentRequest.
-          where("date_format(#{Icm::IncidentRequest.table_name}.submitted_date, '%Y-%m-%d') <= ?",
-                today.strftime('%Y-%m-%d')).
-          where("#{Icm::IncidentRequest.table_name}.external_system_id = ?", params[:external_system_id]).
-          where("NOT EXISTS(SELECT 1 FROM icm_incident_journals ij WHERE ij.incident_request_id = #{Icm::IncidentRequest.table_name}.id AND ij.reply_type = 'CLOSE' AND DATE_FORMAT(ij.created_at, '%Y-%m-%d') <= ?)",
-                today.strftime('%Y-%m-%d'))
-      e = e.where("#{Icm::IncidentRequest.table_name}.support_group_id = ?", params[:support_group_id]) unless params[:support_group_id].blank?
-      data[4] = e.size.to_s
+        e = Icm::IncidentRequest.
+            where("date_format(#{Icm::IncidentRequest.table_name}.submitted_date, '%Y-%m-%d') <= ?",
+                  today.strftime('%Y-%m-%d')).where("#{Icm::IncidentRequest.table_name}.support_group_id = ?", params[:support_group_id]).
+            where("#{Icm::IncidentRequest.table_name}.external_system_id = ?", params[:external_system_id]).
+            where("NOT EXISTS(SELECT 1 FROM icm_incident_journals ij WHERE ij.incident_request_id = #{Icm::IncidentRequest.table_name}.id AND ij.reply_type = 'CLOSE' AND DATE_FORMAT(ij.created_at, '%Y-%m-%d') <= ?)",
+                  today.strftime('%Y-%m-%d'))
+        data[4] = e.size.to_s
+      else
+        n = Icm::IncidentRequest.
+            where("date_format(#{Icm::IncidentRequest.table_name}.submitted_date, '%Y-%m-%d') = ?",
+                  today.strftime('%Y-%m-%d')).
+            where("#{Icm::IncidentRequest.table_name}.external_system_id = ?", params[:external_system_id])
+        data[2] = n.size.to_s
+
+        c = Icm::IncidentRequest.
+            where("EXISTS(SELECT 1 FROM icm_incident_journals ij WHERE ij.incident_request_id = #{Icm::IncidentRequest.table_name}.id AND ij.reply_type = 'CLOSE' AND  DATE_FORMAT(ij.created_at, '%Y-%m-%d') = ?)",
+                  today.strftime('%Y-%m-%d')).
+            where("#{Icm::IncidentRequest.table_name}.external_system_id = ?", params[:external_system_id])
+        data[3] = c.size.to_s
+
+        e = Icm::IncidentRequest.
+            where("date_format(#{Icm::IncidentRequest.table_name}.submitted_date, '%Y-%m-%d') <= ?",
+                  today.strftime('%Y-%m-%d')).
+            where("#{Icm::IncidentRequest.table_name}.external_system_id = ?", params[:external_system_id]).
+            where("NOT EXISTS(SELECT 1 FROM icm_incident_journals ij WHERE ij.incident_request_id = #{Icm::IncidentRequest.table_name}.id AND ij.reply_type = 'CLOSE' AND DATE_FORMAT(ij.created_at, '%Y-%m-%d') <= ?)",
+                  today.strftime('%Y-%m-%d'))
+        data[4] = e.size.to_s
+      end
       datas << data
     end
 
