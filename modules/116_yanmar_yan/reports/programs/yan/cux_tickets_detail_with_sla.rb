cb325af340f0f18de53907d7addb6e6cf4e570f7
stanley <stanley.liu@hand-china.com>
2015-01-25 12:53:47 +0800

    Feature: YAN

--- modules/116_yanmar_yan/reports/programs/yan/cux_tickets_detail_with_sla.rb
+++ modules/116_yanmar_yan/reports/programs/yan/cux_tickets_detail_with_sla.rb
@@ -17,8 +17,8 @@ class Yan::CuxTicketsDetailWithSla < Irm::ReportManager::ReportBase
         with_report_source(I18n.locale).
         with_external_system(I18n.locale).
         select("(SELECT COUNT(1) FROM icm_incident_histories iihh WHERE iihh.new_value <> '000Q00091nxNqRI7bBNZXU' AND iihh.request_id = #{Icm::IncidentRequest.table_name}.id) no_sd").
-        select("(SELECT ssi1.current_duration FROM slm_sla_instances ssi1, slm_sla_agreements_vl ssav1 WHERE ssav1.id = ssi1.service_agreement_id AND ssi1.bo_id = #{Icm::IncidentRequest.table_name}.id AND ssav1.name = '【問合せ】受付遵守率' AND ssav1.external_system_id = #{Icm::IncidentRequest.table_name}.external_system_id ORDER BY ssi1.created_at DESC LIMIT 1) sf").
-        select("(SELECT ssi2.current_duration FROM slm_sla_instances ssi2, slm_sla_agreements_vl ssav2 WHERE ssav2.id = ssi2.service_agreement_id AND ssi2.bo_id = #{Icm::IncidentRequest.table_name}.id AND ssav2.name = '【問合せ】回答所要時間' AND ssav2.external_system_id = #{Icm::IncidentRequest.table_name}.external_system_id ORDER BY ssi2.created_at DESC LIMIT 1) hd").
+        select("(SELECT ssi1.current_duration FROM slm_sla_instances ssi1, slm_service_agreements_vl ssav1 WHERE ssav1.id = ssi1.service_agreement_id AND ssi1.bo_id = #{Icm::IncidentRequest.table_name}.id AND ssav1.name = '【問合せ】受付遵守率' AND ssav1.external_system_id = #{Icm::IncidentRequest.table_name}.external_system_id ORDER BY ssi1.created_at DESC LIMIT 1) sf").
+        select("(SELECT ssi2.current_duration FROM slm_sla_instances ssi2, slm_service_agreements_vl ssav2 WHERE ssav2.id = ssi2.service_agreement_id AND ssi2.bo_id = #{Icm::IncidentRequest.table_name}.id AND ssav2.name = '【問合せ】回答所要時間' AND ssav2.external_system_id = #{Icm::IncidentRequest.table_name}.external_system_id ORDER BY ssi2.created_at DESC LIMIT 1) hd").
         order("(#{Icm::IncidentRequest.table_name}.submitted_date) ASC")
 
     if params[:end_date].present?
