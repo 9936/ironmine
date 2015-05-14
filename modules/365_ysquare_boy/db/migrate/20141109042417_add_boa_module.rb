86a3f4454ba891c1f2ebd5d2679870ae6b11b63b
miracle-li <Jerry@Jerry-Mac.local>
2015-05-14 22:25:03 +0800

    修改square添加到product的migrate

--- modules/365_ysquare_boy/db/migrate/20141109042417_add_boa_module.rb
+++ modules/365_ysquare_boy/db/migrate/20141109042417_add_boa_module.rb
@@ -1,8 +1,8 @@
 class AddBoaModule < ActiveRecord::Migration
   def up
-    irm_product = Irm::ProductModule.new({:product_short_name=>"BOA",:installed_flag=>"Y",:not_auto_mult=>true})
-    irm_product.product_modules_tls.build({:name=>"BOA",:description=>"BOA",:language=>"en",:source_lang=>"en"})
-    irm_product.product_modules_tls.build({:name=>"BOA",:description=>"BOA",:language=>"zh",:source_lang=>"en"})
+    irm_product = Irm::ProductModule.new({:product_short_name=>"BOY",:installed_flag=>"Y",:not_auto_mult=>true})
+    irm_product.product_modules_tls.build({:name=>"BOY",:description=>"BOY",:language=>"en",:source_lang=>"en"})
+    irm_product.product_modules_tls.build({:name=>"BOY",:description=>"BOY",:language=>"zh",:source_lang=>"en"})
     irm_product.save
   end
 
