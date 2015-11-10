23a840d794fa419d64da1146a8ef05a609301743
苗庆新 <qingxin.miao@hand-china.com>
2015-11-10 15:17:14 +0800

    工时开始时间修正

--- modules/116_yanmar_yan/lib/yan/incident_journals_controller_ex.rb
+++ modules/116_yanmar_yan/lib/yan/incident_journals_controller_ex.rb
@@ -206,8 +206,8 @@ module Yan::IncidentJournalsControllerEx
 
                 # 找出当前支持人员所在当前状态的开始时间
                 start_time = nil
-                status_time_record = Icm::IncidentHistory.where("request_id = '#{@incident_journal.incident_request_id}' AND property_key = 'incident_status_id' AND (new_value='000K00091nRTl3hfwbJuHg' OR new_value='000K00091oEOpAuVx0QTVQ' OR new_value='000K00091nRTl3hfuk332W')").order("created_at DESC").first
-                supporter_time_record = Icm::IncidentHistory.where("request_id = '#{@incident_journal.incident_request_id}'").order("created_at DESC").first
+                status_time_record = Icm::IncidentHistory.where("request_id = '#{@incident_journal.incident_request_id}' AND property_key = 'support_person_id' ").order("created_at DESC").first
+                supporter_time_record = Icm::IncidentHistory.where("request_id = '#{@incident_journal.incident_request_id}' AND property_key = 'new_reply' ").order("created_at DESC").first
 
                 if supporter_time_record.present? && status_time_record.present?
                   if supporter_time_record.created_at > status_time_record.created_at
