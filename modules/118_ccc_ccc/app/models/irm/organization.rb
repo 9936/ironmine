7362ddf31c7fa8f457ee82d60523b21eb6cb6c8b
苗庆新 <qingxin.miao@hand-china.com>
2015-12-23 10:44:30 +0800

    公司必填项修改

--- modules/118_ccc_ccc/app/models/irm/organization.rb
+++ modules/118_ccc_ccc/app/models/irm/organization.rb
@@ -14,11 +14,11 @@ class Irm::Organization < ActiveRecord::Base
   belongs_to :industry
   belongs_to :conn_type
 
-  validates_presence_of :router,:industry_id,:conn_type_id,:conscientious,:conscientious_tel,:conscientious_email
+  validates_presence_of :industry_id,:conn_type_id
   validates_uniqueness_of :address,:scope=>[:opu_id], :if => Proc.new { |i| i.address.present? }
 
-  validates_uniqueness_of :conscientious_email, :if => Proc.new { |i| !i.conscientious_email.blank? }, :if => :need_uniqueness_email
-  validates_format_of :conscientious_email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,:message=>:email
+  # validates_uniqueness_of :conscientious_email, :if => Proc.new { |i| !i.conscientious_email.blank? }, :if => :need_uniqueness_email
+  # validates_format_of :conscientious_email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,:message=>:email,:if => Proc.new { |i| !i.conscientious_email.blank? }
 
 
   def need_uniqueness_email
