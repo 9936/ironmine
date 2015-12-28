fcb8f882383bd8e6fc921f6301f2f5f5aeac9b2e
苗庆新 <qingxin.miao@hand-china.com>
2015-12-28 23:02:12 +0800

    注释迁移文件

--- modules/228_board_ccc_boc/db/migrate/20151224143411_create_boc_boards.rb
+++ modules/228_board_ccc_boc/db/migrate/20151224143411_create_boc_boards.rb
@@ -1,16 +1,16 @@
 class CreateBocBoards < ActiveRecord::Migration
   def change
-    create_table :ccc_prime_board_logs do |t|
-      t.string   "created_date",    :limit => 8
-      t.string   "today_create",    :limit => 8
-      t.string   "today_close",    :limit => 8
-      t.string   "today_avg_create",    :limit => 8
-      t.string   "today_avg_close",    :limit => 8
-      t.string   "created_by",        :limit => 22, :collate=>"utf8_bin"
-      t.string   "updated_by",        :limit => 22, :collate=>"utf8_bin"
-      t.datetime "created_at"
-      t.datetime "updated_at"
-    end
-    change_column :ccc_prime_board_logs, "id", :string,:limit=>22, :collate=>"utf8_bin"
+    # create_table :ccc_prime_board_logs do |t|
+    #   t.string   "created_date",    :limit => 8
+    #   t.string   "today_create",    :limit => 8
+    #   t.string   "today_close",    :limit => 8
+    #   t.string   "today_avg_create",    :limit => 8
+    #   t.string   "today_avg_close",    :limit => 8
+    #   t.string   "created_by",        :limit => 22, :collate=>"utf8_bin"
+    #   t.string   "updated_by",        :limit => 22, :collate=>"utf8_bin"
+    #   t.datetime "created_at"
+    #   t.datetime "updated_at"
+    # end
+    # change_column :ccc_prime_board_logs, "id", :string,:limit=>22, :collate=>"utf8_bin"
   end
 end
