e2eaaa04356a3745a5bbc1a85b3d30d994c66727
胡亮 <liang.hu@hand-china.com>
2016-03-29 17:27:24 +0800

    看板微调

--- modules/228_board_ccc_boc/app/controllers/boc/boards_controller.rb
+++ modules/228_board_ccc_boc/app/controllers/boc/boards_controller.rb
@@ -28,22 +28,22 @@ class Boc::BoardsController < ApplicationController
             where("EXISTS (SELECT 1 FROM icm_incident_journals ij WHERE ij.reply_type = 'CLOSE' AND ij.incident_request_id = #{Icm::IncidentRequest.table_name}.id AND DATE_FORMAT(ij.created_at, '%Y-%m-%d') = ?)",
                   today.strftime('%Y-%m-%d')).size
         today_avg_create = Icm::IncidentRequest.
-            where("DATE_FORMAT(#{Icm::IncidentRequest.table_name}.submitted_date, '%Y-%m-%d') >= ?", (today - 13.day).strftime('%Y-%m-%d')).
+            where("DATE_FORMAT(#{Icm::IncidentRequest.table_name}.submitted_date, '%Y-%m-%d') >= ?", (today - 6.day).strftime('%Y-%m-%d')).
             where("DATE_FORMAT(#{Icm::IncidentRequest.table_name}.submitted_date, '%Y-%m-%d') <= ?", today.strftime('%Y-%m-%d')).
             where("#{Icm::IncidentRequest.table_name}.external_system_id IS NOT NULL").
             where("#{Icm::IncidentRequest.table_name}.external_system_id <> '--- Please Select ---'").size
 
-        today_avg_create = (today_avg_create.to_f/14).round(2)
+        today_avg_create = (today_avg_create.to_f/7).round(2)
 
         today_avg_close = Icm::IncidentRequest.
             joins("LEFT OUTER JOIN #{Icm::IncidentCategory.view_name} ic ON ic.id = #{Icm::IncidentRequest.table_name}.incident_category_id AND ic.language = 'zh'").
             enabled.
             where("EXISTS (SELECT 1 FROM icm_incident_journals ij WHERE ij.reply_type = 'CLOSE' AND ij.incident_request_id = #{Icm::IncidentRequest.table_name}.id AND DATE_FORMAT(ij.created_at, '%Y-%m-%d') >= ?)",
-                  (today - 13.day).strftime('%Y-%m-%d')).
+                  (today - 6.day).strftime('%Y-%m-%d')).
             where("EXISTS (SELECT 1 FROM icm_incident_journals ij WHERE ij.reply_type = 'CLOSE' AND ij.incident_request_id = #{Icm::IncidentRequest.table_name}.id AND DATE_FORMAT(ij.created_at, '%Y-%m-%d') <= ?)",
                   today.strftime('%Y-%m-%d')).size
 
-        today_avg_close = (today_avg_close.to_f/14).round(2)
+        today_avg_close = (today_avg_close.to_f/7).round(2)
 
         logs = ActiveRecord::Base.connection.execute("SELECT today_create `0`, today_close `1`, today_avg_create `2`, today_avg_close `3`, created_date `4` FROM ccc_prime_board_logs WHERE created_date = '#{today.strftime('%Y%m%d')}'")
 
@@ -82,19 +82,19 @@ class Boc::BoardsController < ApplicationController
         where("EXISTS (SELECT 1 FROM icm_incident_journals ij WHERE ij.reply_type = 'CLOSE' AND ij.incident_request_id = #{Icm::IncidentRequest.table_name}.id AND DATE_FORMAT(ij.created_at, '%Y-%m-%d') = ?)",
               Time.now.strftime('%Y-%m-%d')).size
     today_avg_create = Icm::IncidentRequest.
-        where("DATE_FORMAT(#{Icm::IncidentRequest.table_name}.submitted_date, '%Y-%m-%d') >= ?", (Time.now - 13.day).strftime('%Y-%m-%d')).
+        where("DATE_FORMAT(#{Icm::IncidentRequest.table_name}.submitted_date, '%Y-%m-%d') >= ?", (Time.now - 7.day).strftime('%Y-%m-%d')).
         where("#{Icm::IncidentRequest.table_name}.external_system_id IS NOT NULL").
         where("#{Icm::IncidentRequest.table_name}.external_system_id <> '--- Please Select ---'").size
 
-    today_avg_create = (today_avg_create.to_f/14).round(2)
+    today_avg_create = (today_avg_create.to_f/7).round(2)
 
     today_avg_close = Icm::IncidentRequest.
         joins("LEFT OUTER JOIN #{Icm::IncidentCategory.view_name} ic ON ic.id = #{Icm::IncidentRequest.table_name}.incident_category_id AND ic.language = 'zh'").
         enabled.
         where("EXISTS (SELECT 1 FROM icm_incident_journals ij WHERE ij.reply_type = 'CLOSE' AND ij.incident_request_id = #{Icm::IncidentRequest.table_name}.id AND DATE_FORMAT(ij.created_at, '%Y-%m-%d') >= ?)",
-              (Time.now - 13.day).strftime('%Y-%m-%d')).size
+              (Time.now - 7.day).strftime('%Y-%m-%d')).size
 
-    today_avg_close = (today_avg_close.to_f/14).round(2)
+    today_avg_close = (today_avg_close.to_f/7).round(2)
 
     @daily_open << today_create
     @daily_close << today_close
