db9c13d575765b92cdb82a3f42437014970e84d2
胡亮 <liang.hu@hand-china.com>
2016-06-03 13:50:23 +0800

    查看顾问在线状态

--- modules/116_yanmar_yan/app/helpers/yan/people_states_helper.rb
+++ modules/116_yanmar_yan/app/helpers/yan/people_states_helper.rb
@@ -1,2 +1,56 @@
 module Yan::PeopleStatesHelper
+  def init_available_people
+    html = ''
+    # 搜索逻辑--->若该顾问没有对应30或者50状态的单子且在线则符合条件
+    Irm::Person.enabled.
+        joins("LEFT OUTER JOIN yan_people_states yps on yps.person_id = #{Irm::Person.table_name}.id").
+        where("NOT EXISTS (
+            SELECT * from icm_incident_requests
+            where support_person_id = #{Irm::Person.table_name}.id
+            and incident_status_id in ('000K00091nRTl3hfwbJuHg','000K00091oEOpAuVx0QTVQ')
+          )
+          AND #{Irm::Person.table_name}.email_address like '%hand-china.com%'
+          and yps.state = 'ENABLED'
+          and yps.updated_at >= ?
+        ",Time.now - 5.minutes).collect { |p|
+      html += "<tr><td>#{p.full_name}</td></tr>"
+    }
+    html.html_safe
+  end
+
+  def init_busy_people
+    html = ''
+    # 搜索逻辑--->若该顾问有对应30或者50状态的单子且在线则符合条件
+    Irm::Person.enabled.
+        joins("LEFT OUTER JOIN yan_people_states yps on yps.person_id = #{Irm::Person.table_name}.id").
+        where("EXISTS (
+            SELECT * from icm_incident_requests
+            where support_person_id = #{Irm::Person.table_name}.id
+            and incident_status_id in ('000K00091nRTl3hfwbJuHg','000K00091oEOpAuVx0QTVQ')
+          )
+          AND #{Irm::Person.table_name}.email_address like '%hand-china.com%'
+          and yps.state = 'ENABLED' and yps.updated_at >= ?
+        ",Time.now - 5.minutes).collect { |p|
+       html += "<tr><td>#{p.full_name}</td></tr>"
+    }
+    html.html_safe
+  end
+
+  def init_offline_people
+    html = ''
+    Irm::Person.enabled.
+        joins("LEFT OUTER JOIN yan_people_states yps on yps.person_id = #{Irm::Person.table_name}.id").
+        # where("NOT EXISTS (
+        #     SELECT * from icm_incident_requests
+        #     where support_person_id = #{Irm::Person.table_name}.id
+        #     and incident_status_id in ('000K00091nRTl3hfwbJuHg','000K00091oEOpAuVx0QTVQ')
+        #   )").
+        where("#{Irm::Person.table_name}.email_address like '%hand-china.com%'").
+        where("yps.state = 'DISABLED' or (yps.state = 'ENABLED' and yps.updated_at < ?)",Time.now - 5.minutes).collect { |p|
+      html += "<tr><td>#{p.full_name}</td></tr>"
+    }
+
+    html.html_safe
+  end
+
 end
