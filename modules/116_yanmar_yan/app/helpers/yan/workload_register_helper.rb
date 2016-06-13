a34e467f38ed9eb8a71580ab8cea6e7419227908
胡亮 <liang.hu@hand-china.com>
2016-06-13 19:27:34 +0800

    登记工时页面模态框调整

--- modules/116_yanmar_yan/app/helpers/yan/workload_register_helper.rb
+++ modules/116_yanmar_yan/app/helpers/yan/workload_register_helper.rb
@@ -1,10 +1,3 @@
 module Yan::WorkloadRegisterHelper
-
-  #xheditor编辑器
-  def new_xheditor(textarea_id,force_fit_width=false,shortcuts = false)
-    unless limit_device?
-      require_jscss(:xheditor)
-      render :partial=>"yan/helper/xheditor",:locals=>{:textarea_id=>textarea_id,:force_fit_width=>force_fit_width, :shortcuts => shortcuts}
-    end
-  end
+  
 end
